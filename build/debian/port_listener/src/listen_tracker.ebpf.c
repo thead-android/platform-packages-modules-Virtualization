49ae797bdb109fa901a2137b47d913be10a3a0a8
Seungjae Yoo <seungjaeyoo@google.com>
2024-10-04 13:44:58 +0900

    Make port_listener buildable
    Bug: 340126051
    Test: cd build/debian/port_listener && sudo ./build.sh
    Change-Id: I01d54c6f5ef7c1b2f4706eedfbfea7d8c3064bd5

--- build/debian/port_listener/src/listen_tracker.ebpf.c
+++ build/debian/port_listener/src/listen_tracker.ebpf.c
@@ -16,11 +16,11 @@
 // src/platform2/vm_tools/port_listener/listen_tracker.ebpf.c
 
 // bpf_helpers.h uses types defined here
-#include "include/vm_tools/port_listener/vmlinux/vmlinux.h"
+#include "vmlinux.h"
 
 #include <bpf/bpf_helpers.h>
 
-#include "vm_tools/port_listener/common.h"
+#include "common.h"
 
 // For some reason 6.1 doesn't include these symbols in the debug build
 // so they don't get included in vmlinux.h. These features have existed since
@@ -29,55 +29,53 @@
 #define BPF_ANY 0
 
 struct {
-  __uint(type, BPF_MAP_TYPE_RINGBUF);
-  __uint(max_entries, 1 << 24);
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
 } events SEC(".maps");
 
 struct {
-  __uint(type, BPF_MAP_TYPE_HASH);
-  __type(key, struct sock*);
-  __type(value, __u8);
-  __uint(max_entries, 65535);
-  __uint(map_flags, BPF_F_NO_PREALLOC);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, struct sock*);
+    __type(value, __u8);
+    __uint(max_entries, 65535);
+    __uint(map_flags, BPF_F_NO_PREALLOC);
 } sockmap SEC(".maps");
 
 const __u8 set_value = 0;
 
 SEC("tp/sock/inet_sock_set_state")
-int tracepoint_inet_sock_set_state(
-    struct trace_event_raw_inet_sock_set_state* ctx) {
-  // We don't support anything other than TCP.
-  if (ctx->protocol != IPPROTO_TCP) {
-    return 0;
-  }
-  struct sock* sk = (struct sock*)ctx->skaddr;
-  // If we're transitioning away from LISTEN but we don't know about this
-  // socket yet then don't report anything.
-  if (ctx->oldstate == BPF_TCP_LISTEN &&
-      bpf_map_lookup_elem(&sockmap, &sk) == NULL) {
-    return 0;
-  }
-  // If we aren't transitioning to or from TCP_LISTEN then we don't care.
-  if (ctx->newstate != BPF_TCP_LISTEN && ctx->oldstate != BPF_TCP_LISTEN) {
-    return 0;
-  }
+int tracepoint_inet_sock_set_state(struct trace_event_raw_inet_sock_set_state* ctx) {
+    // We don't support anything other than TCP.
+    if (ctx->protocol != IPPROTO_TCP) {
+        return 0;
+    }
+    struct sock* sk = (struct sock*)ctx->skaddr;
+    // If we're transitioning away from LISTEN but we don't know about this
+    // socket yet then don't report anything.
+    if (ctx->oldstate == BPF_TCP_LISTEN && bpf_map_lookup_elem(&sockmap, &sk) == NULL) {
+        return 0;
+    }
+    // If we aren't transitioning to or from TCP_LISTEN then we don't care.
+    if (ctx->newstate != BPF_TCP_LISTEN && ctx->oldstate != BPF_TCP_LISTEN) {
+        return 0;
+    }
 
-  struct event* ev;
-  ev = bpf_ringbuf_reserve(&events, sizeof(*ev), 0);
-  if (!ev) {
+    struct event* ev;
+    ev = bpf_ringbuf_reserve(&events, sizeof(*ev), 0);
+    if (!ev) {
+        return 0;
+    }
+    ev->port = ctx->sport;
+
+    if (ctx->newstate == BPF_TCP_LISTEN) {
+        bpf_map_update_elem(&sockmap, &sk, &set_value, BPF_ANY);
+        ev->state = kPortListenerUp;
+    }
+    if (ctx->oldstate == BPF_TCP_LISTEN) {
+        bpf_map_delete_elem(&sockmap, &sk);
+        ev->state = kPortListenerDown;
+    }
+    bpf_ringbuf_submit(ev, 0);
+
     return 0;
-  }
-  ev->port = ctx->sport;
-
-  if (ctx->newstate == BPF_TCP_LISTEN) {
-    bpf_map_update_elem(&sockmap, &sk, &set_value, BPF_ANY);
-    ev->state = kPortListenerUp;
-  }
-  if (ctx->oldstate == BPF_TCP_LISTEN) {
-    bpf_map_delete_elem(&sockmap, &sk);
-    ev->state = kPortListenerDown;
-  }
-  bpf_ringbuf_submit(ev, 0);
-
-  return 0;
 }
