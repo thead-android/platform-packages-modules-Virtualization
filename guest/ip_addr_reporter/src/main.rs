f059c92dc0e79bfc318ef30f04d3c80625f41485
Jeongik Cha <jeongik@google.com>
2024-11-01 20:02:45 +0900

    Use random port for grpc service
    To prevent port conflicts, randomly assign port number to grpc service
    and then share it with the guest via virtiofs
    
    Test: run terminal, and check if grpc works well
    Bug: 372666638
    Change-Id: I90bb430047afd1ba3569d2baa550586a37b26ac7

--- guest/ip_addr_reporter/src/main.rs
+++ guest/ip_addr_reporter/src/main.rs
@@ -1,17 +1,27 @@
 use api::debian_service_client::DebianServiceClient;
 use api::IpAddr;
 
+use clap::Parser;
 pub mod api {
     tonic::include_proto!("com.android.virtualization.vmlauncher.proto");
 }
 
+#[derive(Parser)]
+/// Flags for running command
+pub struct Args {
+    /// grpc port number
+    #[arg(long)]
+    #[arg(alias = "grpc_port")]
+    grpc_port: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), String> {
+    let args = Args::parse();
     let gateway_ip_addr = netdev::get_default_gateway()?.ipv4[0];
     let ip_addr = netdev::get_default_interface()?.ipv4[0].addr();
-    const PORT: i32 = 12000;
 
-    let server_addr = format!("http://{}:{}", gateway_ip_addr.to_string(), PORT);
+    let server_addr = format!("http://{}:{}", gateway_ip_addr.to_string(), args.grpc_port);
 
     println!("local ip addr: {}", ip_addr.to_string());
     println!("coonect to grpc server {}", server_addr);
