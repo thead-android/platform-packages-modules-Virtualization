e08b6ff8fb4891b860c1f446fdde2f17c582ac81
Pierre-Clément Tosi <ptosi@google.com>
2024-11-14 09:37:22 +0000

    Ignore outputIsNotRedirectedToLogcatIfNotDebuggable
    Temporarily ignore the failing test until we've decided if it should be
    removed (b/379029878).
    
    Test: -
    Bug: 372874464
    Change-Id: I5ac6b1ea3967fbe3bfb8e62185e6f1363aec9aef

--- tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
+++ tests/testapk/src/java/com/android/microdroid/test/MicrodroidTests.java
@@ -83,6 +83,7 @@ import com.google.common.truth.BooleanSubject;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
@@ -1935,6 +1936,7 @@ public class MicrodroidTests extends MicrodroidDeviceTestBase {
     }
 
     @Test
+    @Ignore("b/372874464")
     public void outputIsNotRedirectedToLogcatIfNotDebuggable() throws Exception {
         assumeSupportedDevice();
 
