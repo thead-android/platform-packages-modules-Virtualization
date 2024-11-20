3c6ee832470c055935865b6ba80cdc93af881b27
Jaewan Kim <jaewan@google.com>
2024-11-20 13:58:30 +0900

    VmTerminalApp: Implement error activity
    Screenshot is attached to the bug
    
    Bug: 376796062
    Test: Manually. Also checked that back key on error activity doesn't \
    go back to the main activity.
    
    Change-Id: I82713b130a9d25b673b9c9b984077a0d55e4cd92

---
+++ android/TerminalApp/java/com/android/virtualization/terminal/ErrorActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.virtualization.terminal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ErrorActivity extends BaseActivity {
+    public static final String EXTRA_CAUSE = "cause";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_error);
+
+        View button = findViewById(R.id.recovery);
+        button.setOnClickListener((event) -> launchRecoveryActivity());
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Intent intent = getIntent();
+        Exception e = intent.getParcelableExtra(EXTRA_CAUSE, Exception.class);
+        TextView cause = findViewById(R.id.cause);
+        if (e != null) {
+            cause.setText(getString(R.string.error_code, e.toString()));
+        } else {
+            cause.setText(null);
+        }
+    }
+
+    private void launchRecoveryActivity() {
+        Intent intent = new Intent(this, SettingsRecoveryActivity.class);
+        startActivity(intent);
+    }
+}
