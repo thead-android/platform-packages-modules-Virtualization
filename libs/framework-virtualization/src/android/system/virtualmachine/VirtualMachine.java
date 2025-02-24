e7baa2f6fded2c8de867d82c4dbe459871b053bf
Steven Moreland <smoreland@google.com>
2025-02-24 22:27:33 +0000

    AVF: promote FD->IBinder to SystemApi.
    This is needed, dotchya know.
    
    Normally, in the same process, you do all of this, but to
    pass a connection to another process, imagine
    processes "Q -> R -> S", where:
    - Q is client
    - R is VM creator
    - S is VM
    
    Then Q can call binderFromPreconnectedClient, and it can
    implement the provider by calling an API on R which will
    return the results of R calling connectVsock.
    
    This way, you can have direct Q<->S communication, instead of two hops.
    
    Bugs: me
    Test: TH
    Change-Id: If968f18f2c96511fd8359cc68febd01d7dae3833

--- libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
+++ libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
@@ -1970,8 +1970,11 @@ public class VirtualMachine implements AutoCloseable {
      *
      * @hide
      */
+    @SystemApi
+    @SuppressLint("UnflaggedApi") // already existing functionality exposed, users should flag
     public interface VsockConnectionProvider {
         @NonNull
+        @SuppressLint("UnflaggedApi") // already existing functionality exposed, users should flag
         public ParcelFileDescriptor addConnection() throws VirtualMachineException;
     }
 
@@ -2035,6 +2038,8 @@ public class VirtualMachine implements AutoCloseable {
      *
      * @hide
      */
+    @SystemApi
+    @SuppressLint("UnflaggedApi") // already existing functionality exposed, users should flag
     @WorkerThread
     @NonNull
     public static IBinder binderFromPreconnectedClient(@NonNull VsockConnectionProvider provider)
