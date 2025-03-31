aa6887b444787b8c963235cecc2ee9a51f80906d
Daniel Verkamp <dverkamp@google.com>
2025-03-31 14:52:01 -0700

    libvmbase: x86_64: use RDSEED to implement platform_entropy()
    Bug: 375569109
    Test: build and boot pvmfw for x86-64
    Flag: EXEMPT b/375569109
    Change-Id: I0021a1d470dbedb9cf4411925250daa9ca30ba56

--- libs/libvmbase/src/arch/x86_64/rand.rs
+++ libs/libvmbase/src/arch/x86_64/rand.rs
@@ -15,29 +15,82 @@
 //! Random number generator implementation for x86_64
 
 use crate::rand::{Entropy, Error, Result};
+use core::arch::asm;
+use core::arch::x86_64::__cpuid_count;
 use core::fmt;
 
+const RDSEED_MAX_RETRIES: i32 = 10;
+
 /// Error type for rand operations.
-pub struct PlatformError;
+pub enum PlatformError {
+    /// CPU does not have the necessary random number generator instruction.
+    UnsupportedCpu,
+    /// Hardware random number generator is not ready yet.
+    NoEntropy,
+}
 
 impl fmt::Display for PlatformError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "rand error")
+        match self {
+            PlatformError::UnsupportedCpu => write!(f, "unsupported CPU"),
+            PlatformError::NoEntropy => write!(f, "no entropy"),
+        }
     }
 }
 
-pub(crate) const MAX_BYTES_PER_CALL: usize = 1;
+pub(crate) const MAX_BYTES_PER_CALL: usize = size_of::<u64>();
+
+fn rdseed_supported() -> bool {
+    // SAFETY: CPUID itself and CPUID leaf EAX=7,ECX=0 are available on all x86-64 implementations
+    // and has no memory-safety side effects.
+    let extended_features = unsafe { __cpuid_count(7, 0) };
+    extended_features.ebx & (1 << 18) != 0
+}
 
 pub(crate) fn init() -> Result<()> {
-    Ok(())
+    if rdseed_supported() {
+        Ok(())
+    } else {
+        Err(Error::Platform(PlatformError::UnsupportedCpu))
+    }
 }
 
 /// Returns an array where the first `n_bytes` bytes hold entropy.
 ///
 /// The rest of the array should be ignored.
 pub(crate) fn platform_entropy(n_bytes: usize) -> Result<Entropy> {
-    assert_eq!(n_bytes, MAX_BYTES_PER_CALL);
-    // TODO(b/375569109): Provide a proper implementation
-    log::warn!("Platform source of the entropy is not yet implemented!");
-    Ok([42])
+    assert!(n_bytes <= MAX_BYTES_PER_CALL);
+
+    for _ in 0..RDSEED_MAX_RETRIES {
+        if let Some(random_data) = rdseed64() {
+            return Ok(random_data.to_ne_bytes());
+        }
+    }
+
+    Err(Error::Platform(PlatformError::NoEntropy))
+}
+
+// Return 64 bits of random data from the RDSEED instruction, or `None` if the entropy source is not
+// ready yet.
+fn rdseed64() -> Option<u64> {
+    let random_data: u64;
+    let carry_flag: u8;
+
+    // SAFETY: RDSEED is available at all privilege levels and only writes to the output register
+    // and EFLAGS.
+    unsafe {
+        asm!(
+            "rdseed {}",
+            "setc {}",
+            out(reg) random_data,
+            out(reg_byte) carry_flag,
+            options(nomem, nostack),
+        );
+    }
+
+    if carry_flag == 1 {
+        Some(random_data)
+    } else {
+        None
+    }
 }
