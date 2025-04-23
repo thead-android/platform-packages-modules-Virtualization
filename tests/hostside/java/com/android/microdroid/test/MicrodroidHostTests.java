20bdfe723f1fb633eb47edb4f3e551dcaae6648d
ESWAR MAGATAPALLI (xWF) <eswarrao@google.com>
2025-04-23 00:31:43 -0700

    Revert "MicrodroidHostTestCases: Reduce app installs"
    This reverts commit 7a52e8cbbf03046ce81527048a600fb12dcc723a.
    
    Reason for revert: Droidmonitor created revert due to b/412847357. Will be verifying through ABTD before submission
    
    Bug: 409885079
    Change-Id: Id0bf1a8bdfdf85d86787a848b61f112fecb3d711

--- tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
+++ tests/hostside/java/com/android/microdroid/test/MicrodroidHostTests.java
@@ -425,22 +425,15 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
         // Preconditions
         assumeVmTypeSupported("microdroid", true); // Non-protected VMs may not support upgrades
         ensureUpdatableVmSupported();
+        getDevice().uninstallPackage(PACKAGE_NAME);
+        getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ true);
+        ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
 
-        try {
-            // Boot with the default version (V5)
-            ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
-
-            getDevice().uninstallPackage(PACKAGE_NAME);
-            cleanUpVirtualizationTestSetup(getDevice());
-
-            // Install the updated version of app (V6)
-            getDevice().installPackage(findTestFile(APK_UPDATED_NAME), /* reinstall= */ true);
-            ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
-        } finally {
-            // Restore the default MicrodroidTestApp.apk for other tests.
-            getDevice().uninstallPackage(PACKAGE_NAME);
-            getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ false);
-        }
+        getDevice().uninstallPackage(PACKAGE_NAME);
+        cleanUpVirtualizationTestSetup(getDevice());
+        // Install the updated version of app (versionCode 6)
+        getDevice().installPackage(findTestFile(APK_UPDATED_NAME), /* reinstall= */ true);
+        ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
     }
 
     @Test
@@ -451,28 +444,20 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
         // Preconditions: Rollback protection is provided only for protected VM.
         assumeVmTypeSupported("microdroid", true);
 
-        try {
-            // Install the latest version (V6)
-            getDevice().uninstallPackage(PACKAGE_NAME);
-            getDevice().installPackage(findTestFile(APK_UPDATED_NAME), /* reinstall= */ true);
-            ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
+        // Install the upgraded version (v6)
+        getDevice().uninstallPackage(PACKAGE_NAME);
+        getDevice().installPackage(findTestFile(APK_UPDATED_NAME), /* reinstall= */ true);
+        ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG);
 
-            getDevice().uninstallPackage(PACKAGE_NAME);
-            cleanUpVirtualizationTestSetup(getDevice());
-            // Install the default version (V5)
-            getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ true);
+        getDevice().uninstallPackage(PACKAGE_NAME);
+        cleanUpVirtualizationTestSetup(getDevice());
+        // Install the older version (v5)
+        getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ true);
 
-            assertThrows(
-                    "pVM must fail to boot with downgraded payload apk",
-                    DeviceRuntimeException.class,
-                    () ->
-                            ensureProtectedMicrodroidBootsSuccessfully(
-                                    INSTANCE_ID_FILE, INSTANCE_IMG));
-        } finally {
-            // Restore the default MicrodroidTestApp.apk for other tests.
-            getDevice().uninstallPackage(PACKAGE_NAME);
-            getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ false);
-        }
+        assertThrows(
+                "pVM must fail to boot with downgraded payload apk",
+                DeviceRuntimeException.class,
+                () -> ensureProtectedMicrodroidBootsSuccessfully(INSTANCE_ID_FILE, INSTANCE_IMG));
     }
 
     private void ensureProtectedMicrodroidBootsSuccessfully(
@@ -1475,7 +1460,8 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
 
         prepareVirtualizationTestSetup(getDevice());
 
-        new CommandRunner(getDevice()).tryRun("pm", "clear", PACKAGE_NAME);
+        getDevice().installPackage(findTestFile(APK_NAME), /* reinstall= */ false);
+
         new CommandRunner(getDevice())
                 .tryRun(
                         "pm",
@@ -1494,6 +1480,8 @@ public class MicrodroidHostTests extends MicrodroidHostTestCaseBase {
 
         archiveLogThenDelete(
                 mTestLogs, getDevice(), LOG_PATH, "vm.log-" + mTestName.getMethodName());
+
+        getDevice().uninstallPackage(PACKAGE_NAME);
     }
 
     private void assumeVfioPlatformSupported() throws Exception {
