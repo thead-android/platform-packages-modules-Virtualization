254952f628e770e28aaad306d885ec2dd212527d
Jaewan Kim <jaewan@google.com>
2025-05-01 04:28:03 -0700

    Disable `-it` for launching sibling docker in kokoro
    kokoro build fails with following error:
    
    > `the input device is not a TTY`
    
    It's because initial docker-instance is run without `-it`,
    so `build_in_container.sh` couldn't use `-it` as well.
    
    Added `-k` flag to handle kokoro specific behavior.
    
    This also added verbose debug trace for potential debugging.
    
    Bug: 405120095
    Test: Locally launch a docker without `-it`, \
      and verify that `build_in_container.sh` works.
    Flag: EXEMPT test
    Change-Id: I58e6c48621f96ceb69665e6cc9a861fa9aae8302

--- build/debian/build_in_container.sh
+++ build/debian/build_in_container.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+set -x
+
 show_help() {
   echo "Usage: sudo $0 [OPTION]..."
   echo "Builds a debian image and save it to image.raw."
   echo "Options:"
-  echo "-h         Print usage and this help message and exit."
-  echo "-a ARCH    Architecture of the image [default is host arch: $(uname -m)]"
-  echo "-g         Use Debian generic kernel [default is our custom kernel]"
-  echo "-r         Release mode build"
-  echo "-s         Leave a shell open [default: only if the build fails]"
-  echo "-u         Set VM boot mode to u-boot [default is to load kernel directly]"
-  echo "-w         Save temp work directory in the container [for debugging]"
+  echo "-b BUILD_TOP   Specify build top [default is generated from \$ANDROID_BUILD_TOP]"
+  echo "-h             Print usage and this help message and exit."
+  echo "-a ARCH        Architecture of the image [default is host arch: $(uname -m)]"
+  echo "-g             Use Debian generic kernel [default is our custom kernel]"
+  echo "-r             Release mode build"
+  echo "-s             Leave a shell open [default: only if the build fails]"
+  echo "-u             Set VM boot mode to u-boot [default is to load kernel directly]"
+  echo "-w             Save temp work directory in the container [for debugging]"
+  echo "-k             Specify to run inside the kokoro instance"
 }
 
 arch="$(uname -m)"
@@ -19,11 +23,16 @@ release_flag=
 save_workdir_flag=
 shell_condition="||"
 uboot_flag=
+build_top=
+interactive="-it"
 
-while getopts "a:ghrsuw" option; do
+while getopts "a:b:ghrsuw" option; do
   case ${option} in
     a)
       arch="$OPTARG"
+      ;;
+    b)
+      build_top="$OPTARG"
       ;;
     g)
       kernel_flag="-g"
@@ -43,6 +52,9 @@ while getopts "a:ghrsuw" option; do
     w)
       save_workdir_flag="-w"
       ;;
+    k)
+      interactive=""
+      ;;
     *)
       echo "Invalid option: $OPTARG" ; exit 1
       ;;
@@ -53,18 +65,16 @@ if [[ "$arch" != "aarch64" && "$arch" != "x86_64" ]]; then
   echo "Invalid architecture: $arch" ; exit 1
 fi
 
-if [ -n "$AVF_BUILD_TOP" ]; then
-  build_top="$AVF_BUILD_TOP"
-else
+if [ -z "$build_top" ]; then
   if [ -z "$ANDROID_BUILD_TOP" ] ; then
-    echo '`AVF_BUILD_TOP` or `ANDROID_BUILD_TOP` should be undefined.'
-    echo 'Please `lunch` an Android target, or manually set one of the variable.'
+    echo "Cannot find build top"
+    echo "Please use -b option or \'lunch\' an Android target for autodetect"
     exit 1
   fi
   build_top="$ANDROID_BUILD_TOP/packages/modules/Virtualization"
 fi
 
-docker run --privileged -it -v /dev:/dev \
+docker run --privileged $interactive -v /dev:/dev \
   -v "$build_top:/root/Virtualization" \
   --workdir /root/Virtualization/build/debian \
   ubuntu:22.04 \
