70728a0f62bb19bce2c812b52790e0a9c36af349
ELIYAZ MOMIN (xWF) <mohammedeliyaz@google.com>
2025-05-02 04:18:29 -0700

    Revert "Add the VINTF stable IStats service as a supported host ..."
    Revert submission 32745698-stats_vm_client
    
    Reason for revert: <Potential culprit for b/415261623  verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted>
    
    Reverted changes: /q/submissionid:32745698-stats_vm_client
    
    Change-Id: I1b01ab49658d59063ba9ee846ee257603a367f6f

--- tests/vm_accessor/accessor/src/run.rs
+++ tests/vm_accessor/accessor/src/run.rs
@@ -118,7 +118,6 @@ pub fn run_vm() -> Result<VmInstance, Error> {
         payload,
         osName: VM_OS_NAME.to_owned(),
         debugLevel: DebugLevel::FULL,
-        hostServices: vec!["android.frameworks.stats.IStats/default".into()],
         ..Default::default()
     });
 
