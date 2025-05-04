6cb1dd10c84d29bca74efec1f632e85707dc7d23
Jaewan Kim <jaewan@google.com>
2025-05-04 07:54:30 -0700

    kokoro: Fix kokoro environment variable name
    .. and revert the docker image ubuntu:22.04 to avoid auth error.
    
    Test: N/A
    Bug: 405120095
    Flag: EXEMPT not used inside the Android.
    Change-Id: I8627969306952f67f874b10e2eaedcd920ec91a2

--- build/debian/kokoro/gcp_ubuntu_docker/aarch64/build.sh
+++ build/debian/kokoro/gcp_ubuntu_docker/aarch64/build.sh
@@ -7,9 +7,8 @@ sudo losetup -D
 grep vmx /proc/cpuinfo || true
 
 # Sibling docker would be launched from host, so provide host's path for mount.
-AVF_BUILD_TOP="${KOKORO_HOST_ROOT}/src/git/avf"
-sudo ./build_in_container.sh -a aarch64 -b ${AVF_BUILD_TOP} -r -k \
-  -i us-central1-docker.pkg.dev/kokoro-container-bakery/kokoro/ubuntu/ubuntu2204/full:current
+AVF_BUILD_TOP="${KOKORO_HOST_ROOT_DIR}/src/git/avf"
+sudo ./build_in_container.sh -a aarch64 -b ${AVF_BUILD_TOP} -r -k
 
 sudo mv images.tar.gz ${KOKORO_ARTIFACTS_DIR} || true
 mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
