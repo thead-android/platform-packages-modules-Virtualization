cef679c0b2f80628f21aa25c50d2dc4d5463ce4c
Jaewan Kim <jaewan@google.com>
2025-05-08 13:17:39 +0900

    kokoro: Enable sibling docker build on x86_64
    To investigate more about exec format error in aarch64.
    
    Also added to dump kokoro environment variables for more build info.
    
    Bug: 33431244
    Test: N/A
    Flag: EXEMPT exclusively for kokoro
    Change-Id: I31fbe3925f490ca737b61b6a1d498a9ab13a2ebe

--- build/debian/kokoro/gcp_ubuntu_docker/x86_64/build.sh
+++ build/debian/kokoro/gcp_ubuntu_docker/x86_64/build.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+echo "Build info"
+echo "  KOKORO_JOB_NAME = ${KOKORO_JOB_NAME}"
+echo "  KOKORO_BUILD_ID = ${KOKORO_BUILD_ID}"
+echo "  KOKORO_BUILD_NUMBER = ${KOKORO_BUILD_NUMBER}"
+
 cd "${KOKORO_ARTIFACTS_DIR}/git/avf/build/debian/"
 sudo losetup -D
 grep vmx /proc/cpuinfo || true
-sudo ./build.sh -a x86_64 -r
+
+# Sibling docker would be launched from host, so provide host's path for mount.
+AVF_BUILD_TOP="${KOKORO_HOST_ROOT_DIR}/src/git/avf"
+sudo ./build_in_container.sh -a x86_64 -b ${AVF_BUILD_TOP} -r -k
+
 sudo mv images.tar.gz ${KOKORO_ARTIFACTS_DIR} || true
 mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
 sudo cp -r /var/log/fai/* ${KOKORO_ARTIFACTS_DIR}/logs || true
