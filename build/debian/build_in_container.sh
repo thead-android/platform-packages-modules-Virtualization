008153e13bf3dbc0956a4af955bb78ecb55919bf
Jaewan Kim <jaewan@google.com>
2025-05-09 14:52:00 +0900

    Autodetect build top of build_in_container.sh
    Bug: 33431244
    Test: Locally
    Flag: EXEMPT exclusively for kokoro
    Change-Id: Iaa159bfc2c5c83fa5500ea7ecdcee466e0e8d4b2

--- build/debian/build_in_container.sh
+++ build/debian/build_in_container.sh
@@ -2,11 +2,12 @@
 
 set -ex
 
+SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
 show_help() {
   echo "Usage: sudo $0 [OPTION]..."
   echo "Builds a debian image and save it to image.raw."
   echo "Options:"
-  echo "-b BUILD_TOP   Specify build top [default is generated from \$ANDROID_BUILD_TOP]"
   echo "-i IMAGE_NAME  Specify the image name [default is ubuntu:22.04]"
   echo "-h             Print usage and this help message and exit."
   echo "-a ARCH        Architecture of the image [default is host arch: $(uname -m)]"
@@ -18,21 +19,18 @@ show_help() {
 }
 
 arch="$(uname -m)"
+virt_repo_top="${SCRIPT_DIR}/../../"
 kernel_flag=
 release_flag=
 save_workdir_flag=
 shell="|| bash"
 uboot_flag=
-build_top=
 image_name="ubuntu:22.04"
 
-while getopts "a:b:i:ghrsuw" option; do
+while getopts "a:i:ghrsuw" option; do
   case ${option} in
     a)
       arch="$OPTARG"
-      ;;
-    b)
-      build_top="$OPTARG"
       ;;
     i)
       image_name="$OPTARG"
@@ -65,15 +63,6 @@ if [[ "$arch" != "aarch64" && "$arch" != "x86_64" ]]; then
   echo "Invalid architecture: $arch" ; exit 1
 fi
 
-if [ -z "$build_top" ]; then
-  if [ -z "$ANDROID_BUILD_TOP" ] ; then
-    echo "Cannot find build top"
-    echo "Please use -b option or \'lunch\' an Android target for autodetect"
-    exit 1
-  fi
-  build_top="$ANDROID_BUILD_TOP/packages/modules/Virtualization"
-fi
-
 if [[ -t 0 ]]; then
   interactive="-it"
 else
@@ -83,7 +72,7 @@ fi
 
 docker run --privileged $interactive \
   -v /dev:/dev \
-  -v "$build_top:/root/Virtualization" \
+  -v "$virt_repo_top:/root/Virtualization" \
   -v /var/log/fai:/var/log/fai \
   --workdir /root/Virtualization/build/debian \
   "$image_name" \
