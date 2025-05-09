19fe05632c307274d5d91b3fc3a6d8cc92f5bc04
Jaewan Kim <jaewan@google.com>
2025-05-08 23:50:42 -0700

    Revert "kokoro: dump more information in FAI bootstrap"
    This reverts commit ec2467fa4e89187ed37f0ea7e0b0ef23e4ffea09.
    
    Reason for revert: This wasn't helpful for debug FAI failure
    
    Bug: 33431244
    Test: N/A
    Flag: EXEMPT exclusively for kokoro
    Change-Id: I75e346ad20ad6014b41d8d356b65cb4d67cfa33e

--- build/debian/build.sh
+++ build/debian/build.sh
@@ -240,13 +240,7 @@ run_fai() {
 	fi
 
 	local out="${raw_disk_image}"
-	if ! make -C "${debian_cloud_image}" "image_bookworm_nocloud_${debian_arch}"; then
-		# Check freespaces
-		df -h
-		# debootstrap log is only available if FAI failed in deboostrap;
-		# This is the only log that we're interested in at this moment.
-		cat /tmp/fai-diskimage*/debootstrap/debootstrap.log
-	fi
+	make -C "${debian_cloud_image}" "image_bookworm_nocloud_${debian_arch}"
 	mv "${debian_cloud_image}/image_bookworm_nocloud_${debian_arch}.raw" "${out}"
 }
 
