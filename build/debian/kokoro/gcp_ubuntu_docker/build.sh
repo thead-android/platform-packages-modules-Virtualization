ac85edcfa9324978c6998504b1ab67af4d1bc532
Jaewan Kim <jaewan@google.com>
2025-05-09 14:27:05 +0900

    kokoro: Merge build.sh variants
    Bug: 33431244
    Test: Locally
    Flag: EXEMPT exclusively for kokoro
    Change-Id: I793c418779a15c7ee05fa7dac42c071bfcbfd1b1

---
+++ build/debian/kokoro/gcp_ubuntu_docker/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+arch=""
+case "${KOKORO_JOB_NAME}" in # e.g. "ferrochrome/aarch64/continuous
+  *aarch64*)
+    arch="aarch64"
+    # Enable multi-arch container by QEMU for cros-compilation.
+    sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+    ;;
+  *x86_64*)
+    arch="x86_64"
+    ;;
+  *)
+    echo "Unexpected \${KOKORO_JOB_NAME}"
+    echo "Expected to contain architecture, but was ${KOKORO_JOB_NAME}"
+    exit 1
+esac
+
+cd "${KOKORO_ARTIFACTS_DIR}/git/avf/build/debian/"
+
+# Sibling docker would be launched from host, so provide host's path for mount.
+AVF_BUILD_TOP="${KOKORO_HOST_ROOT_DIR}/src/git/avf"
+sudo ./build_in_container.sh -a ${arch} -b ${AVF_BUILD_TOP} -r -w
+
+sudo mv images.tar.gz ${KOKORO_ARTIFACTS_DIR}
+mkdir -p ${KOKORO_ARTIFACTS_DIR}/logs
+sudo cp -r /var/log/fai/* ${KOKORO_ARTIFACTS_DIR}/logs || true
