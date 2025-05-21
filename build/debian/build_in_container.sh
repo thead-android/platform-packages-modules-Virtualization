b65c26a9af30c2f35f49989271614d5a04592171
Saswat Padhi <spadhi@google.com>
2025-05-21 15:21:40 -0700

    build/debian: Minor improvements to containerized build scripts
    The build_custom_kernel_in_container.sh script seems to fail to
    build the kernel independently (outside of the main build script)
    due to a missing dependency (`cpio`).
    
    Adding it in this CL, with several Minor improvements:
    - default virt_repo_top deduced from the script path,
      similar to what's already done in the main build script
    - better handling of docker interactive and shell
    - a `show_help` and alphabetically ordered options
    
    Flag: EXEMPT refactor mostly
    
    Bug: None
    Test: ./build_custom_kernel_in_container.sh -a aarch64
    Test: ./build_custom_kernel_in_container.sh -a x86_64
    Test: ./build_in_container.sh -a aarch64
    Test: ./build_in_container.sh -a x86_64
    Change-Id: Icc3538f0f84a497e1a2900d42b5a663a5a00df27

--- build/debian/build_in_container.sh
+++ build/debian/build_in_container.sh
@@ -5,36 +5,36 @@ set -ex
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
 show_help() {
-  echo "Usage: sudo $0 [OPTION]..."
-  echo "Builds a debian image and save it to image.raw."
+  echo "Usage: $0 [OPTION]..."
+  echo "Builds images.tar.gz with Debian payload."
   echo "Options:"
-  echo "-t VIRT_TOP    Specify the virtualization repo top [default is deduced from script location]"
-  echo "-i IMAGE_NAME  Specify the image name [default is ubuntu:22.04]"
-  echo "-h             Print usage and this help message and exit."
   echo "-a ARCH        Architecture of the image [default is host arch: $(uname -m)]"
+  echo "-b             Set build id [default is eng-\$(hostname)-\$(date --utc)]"
   echo "-g             Use Debian generic kernel [default is our custom kernel]"
+  echo "-h             Print usage and this help message and exit."
+  echo "-i IMAGE_NAME  Specify the image name [default is ubuntu:22.04]"
   echo "-s             Leave a shell open if able [default: only if the build fails]"
+  echo "-t VIRT_TOP    Specify the virtualization repo top [default is deduced from script location]"
   echo "-u             Set VM boot mode to u-boot [default is to load kernel directly]"
   echo "-w             Save temp work directory in the container [for debugging]"
-  echo "-b             Set build id [default is eng-\$(hostname)-\$(date --utc)]"
 }
 
 arch="$(uname -m)"
-virt_repo_top="${SCRIPT_DIR}/../../"
+build_id=$(echo eng-$(hostname)-$(date --utc))
+image_name="ubuntu:22.04"
 kernel_flag=
 save_workdir_flag=
 shell="|| bash"
 uboot_flag=
-image_name="ubuntu:22.04"
-build_id=$(echo eng-$(hostname)-$(date --utc))
+virt_repo_top="${SCRIPT_DIR}/../../"
 
-while getopts "a:b:i:ghsuwt:" option; do
+while getopts "a:b:ghi:st:uw" option; do
   case ${option} in
     a)
       arch="$OPTARG"
       ;;
-    i)
-      image_name="$OPTARG"
+    b)
+      build_id="$OPTARG"
       ;;
     g)
       kernel_flag="-g"
@@ -42,20 +42,20 @@ while getopts "a:b:i:ghsuwt:" option; do
     h)
       show_help ; exit
       ;;
+    i)
+      image_name="$OPTARG"
+      ;;
     s)
       shell="; bash"
+      ;;
+    t)
+      virt_repo_top="$OPTARG"
       ;;
     u)
       uboot_flag="-u"
       ;;
     w)
       save_workdir_flag="-w"
-      ;;
-    b)
-      build_id="$OPTARG"
-      ;;
-    t)
-      virt_repo_top="$OPTARG"
       ;;
     *)
       echo "Invalid option: $OPTARG" ; exit 1
