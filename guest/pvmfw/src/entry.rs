thead-android/platform-packages-modules-Virtualization

commit 172420715c502778586efa83b0d8b7f037d1681a

pvmfw: main: Skip DICE derivation if missing AVB
DICE can only happen with proper inputs, which pvmfw obtains through AVB
so reflect that in the code. Don't pass the DICE handover if to the
guest if none was generated.

This prepares for a future change which will skip AVB.

Note: No function change intended.

Bug: 393977894
Test: m pvmfw
Change-Id: I2be1955236b73b77496141151a78c4c74edd8504

guest/pvmfw/src/entry.rs
@@ -144,7 +144,9 @@ fn main_wrapper<'a>(
         config_entries.vm_dtbo,
         config_entries.vm_ref_dt,
     )?;
-    slices.add_dice_handover(next_dice_handover);
+    if let Some(r) = next_dice_handover {
+        slices.add_dice_handover(r);
+    }
     // Keep UART MMIO_GUARD-ed for debuggable payloads, to enable earlycon.
     let keep_uart = cfg!(debuggable_vms_improvements) && debuggable_payload;
 

guest/pvmfw/src/main.rs
@@ -60,7 +60,7 @@ fn main<'a>(
     mut debug_policy: Option<&[u8]>,
     vm_dtbo: Option<&mut [u8]>,
     vm_ref_dt: Option<&[u8]>,
-) -> Result<(&'a [u8], bool), RebootReason> {
+) -> Result<(Option<&'a [u8]>, bool), RebootReason> {
     info!("pVM firmware");
     debug!("FDT: {:?}", untrusted_fdt.as_ptr());
     debug!("Signed kernel: {:?} ({:#x} bytes)", signed_kernel.as_ptr(), signed_kernel.len());
@@ -81,33 +81,40 @@ fn main<'a>(
         debug_policy = None;
     }
 
-    let (verified_boot_data, debuggable, guest_page_size) =
-        perform_verified_boot(signed_kernel, ramdisk)?;
+    let (verified_boot_data, debuggable, guest_page_size) = {
+        let (dat, debug, sz) = perform_verified_boot(signed_kernel, ramdisk)?;
+        (Some(dat), debug, sz)
+    };
 
     let hyp_page_size = hypervisor_backends::get_granule_size();
     let _ =
         sanitize_device_tree(untrusted_fdt, vm_dtbo, vm_ref_dt, guest_page_size, hyp_page_size)?;
     let fdt = untrusted_fdt; // DT has now been sanitized.
 
-    let instance_hash = salt_from_instance_id(fdt)?;
-    let dice_inputs = PartialInputs::new(&verified_boot_data, instance_hash).map_err(|e| {
-        error!("Failed to compute partial DICE inputs: {e:?}");
-        RebootReason::InternalError
-    })?;
+    let (next_dice_handover, new_instance) = if let Some(ref data) = verified_boot_data {
+        let instance_hash = salt_from_instance_id(fdt)?;
+        let dice_inputs = PartialInputs::new(data, instance_hash).map_err(|e| {
+            error!("Failed to compute partial DICE inputs: {e:?}");
+            RebootReason::InternalError
+        })?;
+        let (new_instance, salt, defer_rollback_protection) =
+            perform_rollback_protection(fdt, data, &dice_inputs, &dice_cdi_seal)?;
+        trace!("Got salt for instance: {salt:x?}");
 
-    let (new_instance, salt, defer_rollback_protection) =
-        perform_rollback_protection(fdt, &verified_boot_data, &dice_inputs, &dice_cdi_seal)?;
-    trace!("Got salt for instance: {salt:x?}");
+        let next_dice_handover = perform_dice_derivation(
+            dice_handover_bytes.as_ref(),
+            dice_context,
+            dice_inputs,
+            &salt,
+            defer_rollback_protection,
+            guest_page_size,
+            guest_page_size,
+        )?;
 
-    let next_dice_handover = perform_dice_derivation(
-        dice_handover_bytes.as_ref(),
-        dice_context,
-        dice_inputs,
-        &salt,
-        defer_rollback_protection,
-        guest_page_size,
-        guest_page_size,
-    )?;
+        (Some(next_dice_handover), new_instance)
+    } else {
+        (None, true)
+    };
 
     let kaslr_seed = u64::from_ne_bytes(rand::random_array().map_err(|e| {
         error!("Failed to generated guest KASLR seed: {e}");

guest/pvmfw/src/fdt.rs
@@ -1360,7 +1360,7 @@ fn patch_device_tree(fdt: &mut Fdt, info: &DeviceTreeInfo) -> Result<(), RebootR
 /// Modifies the input DT according to the fields of the configuration.
 pub fn modify_for_next_stage(
     fdt: &mut Fdt,
-    dice_handover: &[u8],
+    dice_handover: Option<&[u8]>,
     new_instance: bool,
     strict_boot: bool,
     debug_policy: Option<&[u8]>,
@@ -1401,14 +1401,18 @@ pub fn modify_for_next_stage(
 }
 
 /// Patch the "google,open-dice"-compatible reserved-memory node to point to the DICE handover.
-fn patch_dice_node(fdt: &mut Fdt, handover: &[u8]) -> libfdt::Result<()> {
+fn patch_dice_node(fdt: &mut Fdt, handover: Option<&[u8]>) -> libfdt::Result<()> {
     // The node is assumed to be present in the template DT.
     let node = fdt.node_mut(c"/reserved-memory")?.ok_or(FdtError::NotFound)?;
     let mut node = node.next_compatible(c"google,open-dice")?.ok_or(FdtError::NotFound)?;
 
-    let addr = (handover.as_ptr() as usize).try_into().unwrap();
-    let size = handover.len().try_into().unwrap();
-    node.setprop_addrrange_inplace(c"reg", addr, size)
+    if let Some(r) = handover {
+        let addr = (r.as_ptr() as usize).try_into().unwrap();
+        let size = r.len().try_into().unwrap();
+        node.setprop_addrrange_inplace(c"reg", addr, size)
+    } else {
+        node.nop()
+    }
 }
 
 fn empty_or_delete_prop(

commit 572895f65ae011d7032a053b7d007e7f3586738f

pvmfw: Only flush preserved memory if non-empty
Flushing an empty region might trigger a failure and isn't necessary so
use the infrastructure introduced in 172420715c50 ("pvmfw: main: Skip
DICE derivation if missing AVB") to only flush pages populated by pvmfw
for the guest payload if those exist (i.e. their region isn't empty).

Test: TH
Bug: 431154651
Flag: EXEMPT b/406977494
Reported-by: Sreenad Menon <quic_sreemeno@quicinc.com>
Change-Id: I0fec5c3b348d89b0ea5c179c43c9b36491bce80e

guest/pvmfw/src/entry.rs
@@ -24,7 +24,7 @@ use log::LevelFilter;
 use vmbase::{
     bzimage, configure_heap, console_writeln, limit_stack_size, main,
     memory::{
-        map_image_footer, unshare_all_memory, unshare_all_mmio_except_uart, unshare_uart,
+        flush, map_image_footer, unshare_all_memory, unshare_all_mmio_except_uart, unshare_uart,
         MemoryTrackerError, SIZE_256KB, SIZE_4KB,
     },
     power::reboot,
@@ -182,6 +182,7 @@ fn main_wrapper<'a>(argv: &[usize]) -> Result<(NextStage, MemorySlices<'a>), Reb
         config_entries.reserved_mem.as_deref(),
     )?;
     if let Some(r) = preserved_memory {
+        flush(r);
         slices.add_preserved_memory(r);
     }
 

guest/pvmfw/src/main.rs
@@ -53,7 +53,7 @@ use pvmfw_avb::DebugLevel;
 use pvmfw_avb::VerifiedBootData;
 use pvmfw_embedded_key::PUBLIC_KEY;
 use vmbase::heap;
-use vmbase::memory::{flush, SIZE_4KB};
+use vmbase::memory::SIZE_4KB;
 use vmbase::rand;
 use zeroize::Zeroize;
 
@@ -186,8 +186,6 @@ fn main<'a>(
         error!("Failed to configure device tree: {e}");
         RebootReason::InternalError
     })?;
-
-    flush(preserved_memory);
 
     info!("Starting payload...");
     Ok((Some(preserved_memory), debuggable))