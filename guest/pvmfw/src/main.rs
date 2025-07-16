thead-android/platform-packages-modules-Virtualization

commit ac45cd3a22bda59c51a6cf94a05bde21f350576c

pvmfw: Handle 1.3 config.
Adds support for the 1.3 blob to pvmfw which allows for additional
reserved memory blobs to be added to the device tree.

Bug: 359345745
Bug: 382493676
Test: atest libpvmfw.reserved_mem.test; m pvmfw_bin
Flag: EXEMPT b/406977494

Change-Id: I83905e8edd2f716cb52968582aafe1b332658e41

guest/pvmfw/src/main.rs
@@ -31,6 +31,7 @@ mod instance;
 mod memory;
 mod rollback;
 
+use crate::config::reserved_mem::{ResMem, ResMemEntryInfo};
 use crate::dice::{DiceChainInfo, PartialInputs};
 use crate::entry::RebootReason;
 use crate::fdt::{modify_for_next_stage, read_instance_id, sanitize_device_tree};
@@ -39,6 +40,7 @@ use alloc::borrow::Cow;
 use alloc::boxed::Box;
 use alloc::vec::Vec;
 use bssl_avf::Digester;
+use core::slice::ChunksMut;
 use diced_open_dice::{
     bcc_handover_parse, DiceArtifacts, DiceContext, Hidden, HIDDEN_SIZE, VM_KEY_ALGORITHM,
 };
@@ -51,7 +53,9 @@ use pvmfw_embedded_key::PUBLIC_KEY;
 use vmbase::heap;
 use vmbase::memory::{flush, SIZE_4KB};
 use vmbase::rand;
+use zeroize::Zeroize;
 
+#[allow(clippy::too_many_arguments)]
 fn main<'a>(
     untrusted_fdt: &mut Fdt,
     signed_kernel: &[u8],
@@ -60,6 +64,7 @@ fn main<'a>(
     mut debug_policy: Option<&[u8]>,
     vm_dtbo: Option<&mut [u8]>,
     vm_ref_dt: Option<&[u8]>,
+    reserved_mem: Option<&[u8]>,
 ) -> Result<(Option<&'a [u8]>, bool), RebootReason> {
     info!("pVM firmware");
     debug!("FDT: {:?}", untrusted_fdt.as_ptr());
@@ -96,6 +101,43 @@ fn main<'a>(
         sanitize_device_tree(untrusted_fdt, vm_dtbo, vm_ref_dt, guest_page_size, hyp_page_size)?;
     let fdt = untrusted_fdt; // DT has now been sanitized.
 
+    let mut reserved_mem_info: Vec<ResMemEntryInfo> =
+        if let (Some(vb_data), Some(reserved_mem)) = (&verified_boot_data, reserved_mem) {
+            ResMem::new(reserved_mem, guest_page_size)
+                .map_err(|e| {
+                    error!("Failed to parse reserved memory: {e}");
+                    RebootReason::InvalidConfig
+                })?
+                .into_iter()
+                .filter(|x| Some(x.vm_name.to_str().unwrap()) == vb_data.name.as_deref())
+                .collect::<Vec<_>>()
+        } else {
+            Vec::new()
+        };
+
+    let next_dice_handover_pages = if verified_boot_data.is_some() { 1 } else { 0 };
+    let preserved_memory_size =
+        guest_page_size * (reserved_mem_info.len() + next_dice_handover_pages);
+    let preserved_memory = heap::aligned_boxed_slice(preserved_memory_size, guest_page_size)
+        .ok_or_else(|| {
+            error!("Failed to allocate the next-stage reserved memory");
+            RebootReason::InternalError
+        })?;
+
+    // By leaking the slice, its content will be left behind for the next stage.
+    let preserved_memory = Box::leak(preserved_memory);
+    let mut preserved_pages: ChunksMut<'_, u8> = preserved_memory.chunks_mut(guest_page_size);
+
+    // Move reserved memory to preserved preserved_pages.
+    for entry in reserved_mem_info.iter_mut() {
+        let page = preserved_pages.next().unwrap();
+        let (blob, rest) = page.split_at_mut(entry.blob.len());
+        blob.copy_from_slice(entry.blob);
+        rest.zeroize();
+
+        entry.blob = blob;
+    }
+
     let (next_dice_handover, new_instance) = if let Some(ref data) = verified_boot_data {
         let instance_hash = salt_from_instance_id(fdt)?;
         let dice_inputs = PartialInputs::new(data, instance_hash).map_err(|e| {
@@ -108,14 +150,14 @@ fn main<'a>(
             perform_rollback_protection(fdt, data, &dice_inputs, &dice_cdi_seal)?;
         trace!("Got salt for instance: {salt:x?}");
 
-        let next_dice_handover = perform_dice_derivation(
+        let next_dice_handover = preserved_pages.next().unwrap();
+        perform_dice_derivation(
             dice_handover_bytes.as_ref(),
             dice_context,
             dice_inputs,
             &salt,
             defer_rollback_protection,
-            guest_page_size,
-            guest_page_size,
+            next_dice_handover,
         )?;
 
         (Some(next_dice_handover), new_instance)
@@ -130,20 +172,23 @@ fn main<'a>(
     let strict_boot = true;
     modify_for_next_stage(
         fdt,
-        next_dice_handover,
+        next_dice_handover.as_deref(),
         new_instance,
         strict_boot,
         debug_policy,
         debuggable,
         kaslr_seed,
+        &reserved_mem_info,
     )
     .map_err(|e| {
         error!("Failed to configure device tree: {e}");
         RebootReason::InternalError
     })?;
 
+    flush(preserved_memory);
+
     info!("Starting payload...");
-    Ok((next_dice_handover, debuggable))
+    Ok((Some(preserved_memory), debuggable))
 }
 
 fn parse_dice_handover(
@@ -194,23 +239,14 @@ fn parse_dice_handover(
     Ok((Some((bytes_for_next, cdi_seal, dice_context)), is_debug_mode))
 }
 
-fn perform_dice_derivation<'a>(
+fn perform_dice_derivation(
     dice_handover_bytes: &[u8],
     dice_context: DiceContext,
     dice_inputs: PartialInputs,
     salt: &[u8; HIDDEN_SIZE],
     defer_rollback_protection: bool,
-    next_handover_size: usize,
-    next_handover_align: usize,
-) -> Result<&'a [u8], RebootReason> {
-    let next_dice_handover = heap::aligned_boxed_slice(next_handover_size, next_handover_align)
-        .ok_or_else(|| {
-            error!("Failed to allocate the next-stage DICE handover");
-            RebootReason::InternalError
-        })?;
-    // By leaking the slice, its content will be left behind for the next stage.
-    let next_dice_handover = Box::leak(next_dice_handover);
-
+    next_dice_handover: &mut [u8],
+) -> Result<(), RebootReason> {
     dice_inputs
         .write_next_handover(
             dice_handover_bytes.as_ref(),
@@ -223,8 +259,7 @@ fn perform_dice_derivation<'a>(
             error!("Failed to derive next-stage DICE secrets: {e:?}");
             RebootReason::SecretDerivationError
         })?;
-    flush(next_dice_handover);
-    Ok(next_dice_handover)
+    Ok(())
 }
 
 fn perform_verified_boot<'a>(

guest/pvmfw/Android.bp
@@ -130,6 +130,20 @@ rust_test {
     static_libs: ["libopen_dice_clear_memory"],
 }
 
+rust_test {
+    name: "libpvmfw.reserved_mem.test",
+    srcs: ["src/config/reserved_mem.rs"],
+    defaults: ["libpvmfw.test.defaults"],
+    rustlibs: [
+        "liblibfdt",
+        "liblog_rust",
+        "libzerocopy_nostd",
+    ],
+    data: [
+        ":test_pvmfw_devices_without_device",
+    ],
+}
+
 genrule {
     name: "test_pvmfw_devices_vm_dtbo",
     defaults: ["dts_to_dtb"],

guest/pvmfw/src/arch/aarch64/payload.rs
@@ -23,7 +23,7 @@ use vmbase::{arch::aarch64::min_dcache_line_size, layout, memory::deactivate_dyn
 /// Function boot payload after cleaning all secret from pvmfw memory
 pub fn jump_to_payload(entrypoint: usize, slices: &MemorySlices) -> ! {
     let fdt_address = slices.fdt.as_ptr() as usize;
-    let dice_handover = slices.dice_handover.map(|slice| {
+    let preserved_memory = slices.preserved_memory.map(|slice| {
         let r = slice.as_ptr_range();
         (r.start as usize)..(r.end as usize)
     });
@@ -50,7 +50,7 @@ pub fn jump_to_payload(entrypoint: usize, slices: &MemorySlices) -> ! {
     // A sub-region of the scratch memory might contain data for the next stage so skip zeroing it.
     // Alternatively, an empty region at the start of the scratch region is compatible with the ASM
     // implementation and results in the whole scratch region being zeroed.
-    let skipped = dice_handover.unwrap_or(scratch.start.0..scratch.start.0);
+    let skipped = preserved_memory.unwrap_or(scratch.start.0..scratch.start.0);
 
     assert!(skipped.is_within(&(scratch.start.0..scratch.end.0)));
     assert_eq!(skipped.start % ASM_STP_ALIGN, 0, "Misaligned skipped region.");

guest/pvmfw/src/config/mod.rs
@@ -27,6 +27,8 @@ use zerocopy::Immutable;
 use zerocopy::KnownLayout;
 use zerocopy::Ref;
 
+pub mod reserved_mem;
+
 /// Configuration data header.
 #[repr(C, packed)]
 #[derive(Clone, Copy, Debug, FromBytes, Immutable, KnownLayout)]
@@ -88,6 +90,7 @@ impl Header {
     const VERSION_1_0: Version = Version { major: 1, minor: 0 };
     const VERSION_1_1: Version = Version { major: 1, minor: 1 };
     const VERSION_1_2: Version = Version { major: 1, minor: 2 };
+    const VERSION_1_3: Version = Version { major: 1, minor: 3 };
 
     pub fn total_size(&self) -> usize {
         self.total_size as usize
@@ -110,8 +113,9 @@ impl Header {
             Self::VERSION_1_0 => Entry::DebugPolicy,
             Self::VERSION_1_1 => Entry::VmDtbo,
             Self::VERSION_1_2 => Entry::VmBaseDtbo,
+            Self::VERSION_1_3 => Entry::ReservedMem,
             v @ Version { major: 1, .. } => {
-                const LATEST: Version = Header::VERSION_1_2;
+                const LATEST: Version = Header::VERSION_1_3;
                 warn!("Parsing unknown config data version {v} as version {LATEST}");
                 return Ok(Entry::COUNT);
             }
@@ -128,6 +132,7 @@ pub enum Entry {
     DebugPolicy,
     VmDtbo,
     VmBaseDtbo,
+    ReservedMem,
     #[allow(non_camel_case_types)] // TODO: Use mem::variant_count once stable.
     _VARIANT_COUNT,
 }
@@ -136,7 +141,7 @@ impl Entry {
     const COUNT: usize = Self::_VARIANT_COUNT as usize;
 
     const ALL_ENTRIES: [Entry; Self::COUNT] =
-        [Self::DiceHandover, Self::DebugPolicy, Self::VmDtbo, Self::VmBaseDtbo];
+        [Self::DiceHandover, Self::DebugPolicy, Self::VmDtbo, Self::VmBaseDtbo, Self::ReservedMem];
 }
 
 #[derive(Default)]
@@ -145,6 +150,7 @@ pub struct Entries<'a> {
     pub debug_policy: Option<&'a [u8]>,
     pub vm_dtbo: Option<&'a mut [u8]>,
     pub vm_ref_dt: Option<&'a [u8]>,
+    pub reserved_mem: Option<&'a mut [u8]>,
 }
 
 #[repr(packed)]
@@ -293,12 +299,12 @@ impl<'a> Config<'a> {
                 entries[i] = Some(chunk);
             }
         }
-        let [dice_handover, debug_policy, vm_dtbo, vm_ref_dt] = entries;
+        let [dice_handover, debug_policy, vm_dtbo, vm_ref_dt, reserved_mem] = entries;
 
         // We have no reason to mutate so drop the `mut`.
         let debug_policy = debug_policy.map(|x| &*x);
         let vm_ref_dt = vm_ref_dt.map(|x| &*x);
 
-        Entries { dice_handover, debug_policy, vm_dtbo, vm_ref_dt }
+        Entries { dice_handover, debug_policy, vm_dtbo, vm_ref_dt, reserved_mem }
     }
 }

guest/pvmfw/src/config/reserved_mem.rs
@@ -1,0 +1,337 @@
+use core::ffi::CStr;
+use core::fmt;
+use zerocopy::byteorder::U32;
+use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout, NativeEndian};
+
+pub const NO_MAP: u32 = 1u32;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Error {
+    /// The struct indicated a different number of headers than were found.
+    InconsistentSize,
+    /// Data offsets invalid.
+    InvalidOffset(usize),
+    /// Compat string was not a valid CStr.
+    InvalidCStr,
+    /// Passed in buffer too small for any data.
+    InputBufferSmall,
+    /// A header referenced an invalid blob size.
+    InvalidSize(usize),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::InconsistentSize => write!(f, "Header count did not match indicated number"),
+            Self::InvalidOffset(offset) => {
+                write!(f, "Encountered an invalid data offset ({offset})")
+            }
+            Self::InvalidCStr => write!(f, "Encountered an invalid CStr"),
+            Self::InputBufferSmall => write!(f, "Input buffer too small"),
+            Self::InvalidSize(sz) => write!(f, "An entry indicated an invalid size ({sz})"),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(
+    Clone, Copy, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq,
+)]
+pub(crate) struct ResMemEntry {
+    vm_name_offset: U32<NativeEndian>,
+    blob_offset: U32<NativeEndian>,
+    blob_size: U32<NativeEndian>,
+    compat_offset: U32<NativeEndian>,
+    flags: U32<NativeEndian>,
+}
+
+impl ResMemEntry {
+    fn blob_offset(&self) -> usize {
+        self.blob_offset.get() as usize
+    }
+
+    fn blob_size(&self) -> usize {
+        self.blob_size.get() as usize
+    }
+
+    fn compat_offset(&self) -> usize {
+        self.compat_offset.get() as usize
+    }
+
+    fn vm_name_offset(&self) -> usize {
+        self.vm_name_offset.get() as usize
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Eq, Immutable, KnownLayout, PartialEq)]
+pub(crate) struct ResMem<'a> {
+    entries: &'a [ResMemEntry],
+    buffer: &'a [u8],
+}
+
+fn check_cstr(buffer: &[u8]) -> Result<(), Error> {
+    CStr::from_bytes_until_nul(buffer).map(|_| ()).map_err(|_| Error::InvalidCStr)
+}
+
+impl<'a> ResMem<'a> {
+    pub(crate) fn new(buffer: &'a [u8], guest_page_size: usize) -> Result<Self, Error> {
+        let (count, rem) = u32::read_from_prefix(buffer).map_err(|_| Error::InputBufferSmall)?;
+        let (entries, buffer) = <[ResMemEntry]>::ref_from_prefix_with_elems(rem, count as usize)
+            .map_err(|_| Error::InconsistentSize)?;
+
+        let mut last_blob_end = 0;
+
+        for e in entries.iter() {
+            check_cstr(
+                buffer.get(e.compat_offset()..).ok_or(Error::InvalidOffset(e.compat_offset()))?,
+            )?;
+            check_cstr(
+                buffer.get(e.vm_name_offset()..).ok_or(Error::InvalidOffset(e.vm_name_offset()))?,
+            )?;
+
+            // We don't currently have a use case for regions larger than a page and limiting this
+            // simplifies implementation, to regions to one page for now.
+            if e.blob_size() == 0 || e.blob_size() > guest_page_size {
+                return Err(Error::InvalidSize(e.blob_size()));
+            }
+
+            let start = e.blob_offset();
+            let end = start.checked_add(e.blob_size()).ok_or(Error::InvalidOffset(start))?;
+            if start < last_blob_end {
+                return Err(Error::InvalidOffset(start));
+            }
+            if end > buffer.len() {
+                return Err(Error::InvalidOffset(end));
+            }
+            last_blob_end = end;
+        }
+
+        Ok(Self { entries, buffer })
+    }
+}
+
+impl<'a> IntoIterator for ResMem<'a> {
+    type Item = ResMemEntryInfo<'a>;
+    type IntoIter = ResMemIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ResMemIterator { resmem: self, next: 0 }
+    }
+}
+
+pub(crate) struct ResMemIterator<'a> {
+    resmem: ResMem<'a>,
+    next: usize,
+}
+
+pub(crate) struct ResMemEntryInfo<'a> {
+    pub blob: &'a [u8],
+    pub compat: &'a CStr,
+    pub vm_name: &'a CStr,
+    pub flags: u32,
+}
+
+impl<'a> Iterator for ResMemIterator<'a> {
+    type Item = ResMemEntryInfo<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let entry = &self.resmem.entries.get(self.next)?;
+        let compat =
+            CStr::from_bytes_until_nul(&self.resmem.buffer[entry.compat_offset()..]).unwrap();
+        let vm_name =
+            CStr::from_bytes_until_nul(&self.resmem.buffer[entry.vm_name_offset()..]).unwrap();
+        self.next += 1;
+        Some(Self::Item {
+            blob: &self.resmem.buffer[entry.blob_offset()..entry.blob_offset() + entry.blob_size()],
+            compat,
+            vm_name,
+            flags: entry.flags.into(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use zerocopy::ConvertError;
+
+    const PAGE_SIZE: usize = 4096;
+
+    struct TestHeader<const N: usize> {
+        size: u32,
+        entries: [ResMemEntry; N],
+    }
+
+    struct TestBlob<'a>(&'a CStr, Vec<u8>, &'a CStr, u32);
+
+    fn make_test_data<const N: usize>(data: &[TestBlob<'_>; N]) -> (TestHeader<N>, Vec<u8>) {
+        let mut out_hdr = TestHeader { size: N as u32, entries: [Default::default(); N] };
+
+        let mut out_data = vec![];
+        for (i, d) in data.iter().enumerate() {
+            let compat = d.0.to_bytes_with_nul();
+            let blob = d.1.as_slice();
+            let vm_name = d.2.to_bytes_with_nul();
+            let flags = d.3;
+
+            let vm_name_offset = out_data.len() as u32;
+            out_data.extend_from_slice(vm_name);
+            let blob_offset = out_data.len() as u32;
+            out_data.extend_from_slice(blob);
+            let compat_offset = out_data.len() as u32;
+            out_data.extend_from_slice(compat);
+
+            out_hdr.entries[i] =
+                make_header(vm_name_offset, blob_offset, blob.len() as u32, compat_offset, flags);
+        }
+        (out_hdr, out_data)
+    }
+
+    fn make_header(
+        vm_name_offset: u32,
+        blob_offset: u32,
+        blob_size: u32,
+        compat_offset: u32,
+        flags: u32,
+    ) -> ResMemEntry {
+        ResMemEntry {
+            vm_name_offset: vm_name_offset.into(),
+            blob_offset: blob_offset.into(),
+            blob_size: blob_size.into(),
+            compat_offset: compat_offset.into(),
+            flags: flags.into(),
+        }
+    }
+
+    fn make_test_vec<const N: usize>(hdr: TestHeader<N>, mut data: Vec<u8>) -> Vec<u8> {
+        // reuse the `data` allocation
+        data.extend_from_slice(hdr.size.as_bytes());
+        data.extend_from_slice(hdr.entries.as_bytes());
+        data.rotate_right(size_of_val(&hdr));
+        data
+    }
+
+    /// Ensure `data` is aligned on an odd address; output the original data.
+    /// Resizes `data` if necessary.
+    fn dealign_vec_data(data: &mut Vec<u8>) -> &[u8] {
+        assert!(data.len() >= 4);
+        data.reserve(1);
+        let out = if data.as_ptr() as usize % 2 == 0 {
+            data.insert(0, 0);
+            &data.as_slice()[1..]
+        } else {
+            data.as_slice()
+        };
+        assert!(matches!(u16::ref_from_prefix(out), Err(ConvertError::Alignment(_))));
+        out
+    }
+
+    #[test]
+    fn unaligned() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 256], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 256], c"Leela", 0),
+        ];
+        let (hdr, data) = make_test_data(&blobs);
+
+        let mut bytes = make_test_vec(hdr, data);
+
+        assert!(ResMem::new(dealign_vec_data(&mut bytes), PAGE_SIZE).is_ok());
+    }
+
+    #[test]
+    fn success() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 256], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 256], c"Leela", NO_MAP),
+        ];
+        let (hdr, data) = make_test_data(&blobs);
+
+        let bytes = make_test_vec(hdr, data);
+
+        let cfg_header = ResMem::new(bytes.as_slice(), PAGE_SIZE);
+        assert!(cfg_header.is_ok());
+
+        for (i, entry) in cfg_header.unwrap().into_iter().enumerate() {
+            assert_eq!(entry.compat, blobs[i].0);
+            assert_eq!(entry.blob, blobs[i].1);
+            assert_eq!(entry.vm_name, blobs[i].2);
+            assert_eq!(entry.flags, blobs[i].3);
+        }
+    }
+
+    #[test]
+    fn input_too_small() {
+        let data = &[0u8; 3];
+        let cfg_header = ResMem::new(data, PAGE_SIZE);
+        assert_eq!(cfg_header, Err(Error::InputBufferSmall));
+    }
+
+    #[test]
+    fn bad_header_size_too_large() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 256], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 256], c"Leela", 0),
+            TestBlob(c"Baz", vec![0xCC; 64], c"Bender", 0),
+        ];
+        let (mut hdr, data) = make_test_data(&blobs);
+
+        // Set incorrect size.
+        hdr.size = 20000;
+
+        let bytes = make_test_vec(hdr, data);
+
+        let cfg_header = ResMem::new(bytes.as_slice(), PAGE_SIZE);
+        assert_eq!(cfg_header, Err(Error::InconsistentSize));
+    }
+
+    #[test]
+    fn bad_total_size() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 256], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 17], c"Leela", 0),
+            TestBlob(c"Baz", vec![0xCC; 64], c"Bender", 0),
+        ];
+        let (hdr, data) = make_test_data(&blobs);
+
+        let mut bytes: Vec<u8> = vec![];
+        bytes.extend_from_slice(hdr.size.as_bytes());
+        bytes.extend_from_slice(hdr.entries.as_bytes());
+        let len = data.len();
+        bytes.extend_from_slice(&data[..len - 64]);
+
+        let cfg_header = ResMem::new(bytes.as_slice(), PAGE_SIZE);
+        assert_eq!(cfg_header, Err(Error::InvalidOffset(len - core::mem::size_of_val(&hdr.size))));
+    }
+
+    #[test]
+    fn zero_size() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 0], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 17], c"Leela", 0),
+            TestBlob(c"Baz", vec![0xCC; 64], c"Bender", 0),
+        ];
+        let (hdr, data) = make_test_data(&blobs);
+
+        let bytes = make_test_vec(hdr, data);
+
+        let cfg_header = ResMem::new(bytes.as_slice(), PAGE_SIZE);
+        assert_eq!(cfg_header, Err(Error::InvalidSize(0)));
+    }
+
+    #[test]
+    fn size_too_big() {
+        let blobs = [
+            TestBlob(c"Foo", vec![0xAA; 32768], c"Fry", 0),
+            TestBlob(c"Bar", vec![0xBB; 17], c"Leela", 0),
+            TestBlob(c"Baz", vec![0xCC; 64], c"Bender", 0),
+        ];
+        let (hdr, data) = make_test_data(&blobs);
+
+        let bytes = make_test_vec(hdr, data);
+
+        let cfg_header = ResMem::new(bytes.as_slice(), PAGE_SIZE);
+        assert_eq!(cfg_header, Err(Error::InvalidSize(32768)));
+    }
+}

guest/pvmfw/src/entry.rs
@@ -135,7 +135,7 @@ fn main_wrapper<'a>(
     let mut slices = MemorySlices::new(fdt, payload, payload_size)?;
 
     // This wrapper allows main() to be blissfully ignorant of platform details.
-    let (next_dice_handover, debuggable_payload) = crate::main(
+    let (preserved_memory, debuggable_payload) = crate::main(
         slices.fdt,
         slices.kernel,
         slices.ramdisk,
@@ -143,9 +143,10 @@ fn main_wrapper<'a>(
         config_entries.debug_policy,
         config_entries.vm_dtbo,
         config_entries.vm_ref_dt,
+        config_entries.reserved_mem.as_deref(),
     )?;
-    if let Some(r) = next_dice_handover {
-        slices.add_dice_handover(r);
+    if let Some(r) = preserved_memory {
+        slices.add_preserved_memory(r);
     }
 
     // Keep UART MMIO_GUARD-ed for debuggable payloads, to enable earlycon.
@@ -153,6 +154,9 @@ fn main_wrapper<'a>(
 
     // Writable-dirty regions will be flushed when MemoryTracker is dropped.
     if let Some(r) = config_entries.dice_handover {
+        r.zeroize();
+    }
+    if let Some(r) = config_entries.reserved_mem {
         r.zeroize();
     }
 

guest/pvmfw/src/fdt.rs
@@ -15,6 +15,7 @@
 //! High-level FDT functions.
 
 use crate::bootargs::BootArgsIterator;
+use crate::config::reserved_mem::{ResMemEntryInfo, NO_MAP};
 use crate::device_assignment::{self, DeviceAssignmentInfo, VmDtbo};
 use crate::Box;
 use crate::RebootReason;
@@ -1438,6 +1439,7 @@ fn patch_device_tree(fdt: &mut Fdt, info: &DeviceTreeInfo) -> Result<(), RebootR
 }
 
 /// Modifies the input DT according to the fields of the configuration.
+#[allow(clippy::too_many_arguments)]
 pub fn modify_for_next_stage(
     fdt: &mut Fdt,
     dice_handover: Option<&[u8]>,
@@ -1446,6 +1448,7 @@ pub fn modify_for_next_stage(
     debug_policy: Option<&[u8]>,
     debuggable: bool,
     kaslr_seed: u64,
+    reserved_mem: &[ResMemEntryInfo],
 ) -> libfdt::Result<()> {
     if let Some(debug_policy) = debug_policy {
         let backup = Vec::from(fdt.as_slice());
@@ -1472,6 +1475,18 @@ pub fn modify_for_next_stage(
     if !debuggable {
         if let Some(bootargs) = read_bootargs_from(fdt)? {
             filter_out_dangerous_bootargs(fdt, &bootargs)?;
+        }
+    }
+
+    for item in reserved_mem.iter() {
+        let mem_node = fdt.node_mut(c"/reserved-memory")?.ok_or(FdtError::NotFound)?;
+        let name = CString::new(format!("memory@0x{:p}", item.blob.as_ptr())).unwrap();
+        let mut node = mem_node.add_subnode(&name)?;
+
+        node.appendprop(c"compatible", &item.compat.to_bytes_with_nul())?;
+        node.appendprop_addrrange(c"reg", item.blob.as_ptr() as u64, item.blob.len() as u64)?;
+        if item.flags & NO_MAP == NO_MAP {
+            node.setprop_empty(c"no-map")?;
         }
     }
 

guest/pvmfw/src/memory.rs
@@ -31,7 +31,7 @@ pub(crate) struct MemorySlices<'a> {
     pub fdt: &'a mut libfdt::Fdt,
     pub kernel: &'a [u8],
     pub ramdisk: Option<&'a [u8]>,
-    pub dice_handover: Option<&'a [u8]>,
+    pub preserved_memory: Option<&'a [u8]>,
 }
 
 impl<'a> MemorySlices<'a> {
@@ -112,12 +112,12 @@ impl<'a> MemorySlices<'a> {
             None
         };
 
-        let dice_handover = None;
+        let preserved_memory = None;
 
-        Ok(Self { fdt: untrusted_fdt, kernel, ramdisk, dice_handover })
+        Ok(Self { fdt: untrusted_fdt, kernel, ramdisk, preserved_memory })
     }
 
-    pub fn add_dice_handover(&mut self, slice: &'a [u8]) {
-        self.dice_handover = Some(slice)
+    pub fn add_preserved_memory(&mut self, slice: &'a [u8]) {
+        self.preserved_memory = Some(slice)
     }
 }

commit 005f4c6452f70324c626ad1ff327f8152225f487

pvmfw: Fix empty preserved memory alloc for QTVMs
Allocating an empty slice fails, which is only possible when no
"reserved memory" is passed through the config data (v1.3+) and DICE is
skipped (QTVMs only), hence not being caught by our tests.

Test: https://partner-android-review.googlesource.com/c/platform/packages/modules/Virtualization/+/3350201/comments/2f016a95_a9ce30ac
Bug: 431154651
Flag: EXEMPT b/406977494
Fixes: ac45cd3a22bd ("pvmfw: Handle 1.3 config.")
Reported-by: Sreenad Menon <quic_sreemeno@quicinc.com>
Change-Id: I57ece276d266e5f2c99e250ccb24a2b20888ccf1

guest/pvmfw/src/main.rs
@@ -118,16 +118,20 @@ fn main<'a>(
         };
 
     let next_dice_handover_pages = if verified_boot_data.is_some() { 1 } else { 0 };
-    let preserved_memory_size =
-        guest_page_size * (reserved_mem_info.len() + next_dice_handover_pages);
-    let preserved_memory = heap::aligned_boxed_slice(preserved_memory_size, guest_page_size)
-        .ok_or_else(|| {
-            error!("Failed to allocate the next-stage reserved memory");
-            RebootReason::InternalError
-        })?;
+    let preserved_memory_pages = reserved_mem_info.len() + next_dice_handover_pages;
+    let preserved_memory = if preserved_memory_pages > 0 {
+        let preserved_memory_size = preserved_memory_pages * guest_page_size;
+        let slice =
+            heap::aligned_boxed_slice(preserved_memory_size, guest_page_size).ok_or_else(|| {
+                error!("Failed to allocate the next-stage reserved memory");
+                RebootReason::InternalError
+            })?;
 
-    // By leaking the slice, its content will be left behind for the next stage.
-    let preserved_memory = Box::leak(preserved_memory);
+        // By leaking the slice, its content will be left behind for the next stage.
+        Box::leak(slice)
+    } else {
+        &mut []
+    };
     let mut preserved_pages: ChunksMut<'_, u8> = preserved_memory.chunks_mut(guest_page_size);
 
     // Move reserved memory to preserved preserved_pages.
@@ -190,7 +194,12 @@ fn main<'a>(
     flush(preserved_memory);
 
     info!("Starting payload...");
-    Ok((Some(preserved_memory), debuggable))
+
+    if preserved_memory.is_empty() {
+        Ok((None, debuggable))
+    } else {
+        Ok((Some(preserved_memory), debuggable))
+    }
 }
 
 fn parse_dice_handover(