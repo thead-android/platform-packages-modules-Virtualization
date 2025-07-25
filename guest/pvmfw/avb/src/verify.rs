4bbb336fbe6b91a446830b87a0f2a76dcb77664a
Pierre-Clément Tosi <ptosi@google.com>
2025-07-25 09:43:46 -0700

    Revert "pvmfw: rollback: Support fixed-digest RBP"
    This reverts commit f9b864fad9913902e645501642f5e0bab6c05529.
    
    Reason for revert: b/434179455
    
    Bug: 392628867
    Change-Id: I8157b67a8a61cab65d4d0288fd7f7644abc91f7a

--- guest/pvmfw/avb/src/verify.rs
+++ guest/pvmfw/avb/src/verify.rs
@@ -39,8 +39,6 @@ pub struct VerifiedBootData<'a> {
     pub kernel_digest: Digest,
     /// Initrd digest if initrd exists.
     pub initrd_digest: Option<Digest>,
-    /// VBMeta digest.
-    pub vbmeta_digest: Digest,
     /// Trusted public key.
     pub public_key: &'a [u8],
     /// VM capabilities.
@@ -293,7 +291,6 @@ pub fn verify_payload<'a>(
     let capabilities = Capability::get_capabilities(vbmeta_image)?;
     let page_size = read_page_size(vbmeta_image)?;
     let name = read_name(vbmeta_image)?;
-    let vbmeta_digest = kernel_verify_result.calculate_sha256_digest();
 
     if initrd.is_none() {
         hash_descriptors.verify_no_initrd()?;
@@ -301,7 +298,6 @@ pub fn verify_payload<'a>(
             debug_level: DebugLevel::None,
             kernel_digest: copy_digest(hash_descriptors.kernel)?,
             initrd_digest: None,
-            vbmeta_digest,
             public_key: trusted_public_key,
             capabilities,
             rollback_index,
@@ -325,7 +321,6 @@ pub fn verify_payload<'a>(
         kernel_digest: copy_digest(hash_descriptors.kernel)?,
         initrd_digest: Some(copy_digest(initrd_descriptor)?),
         public_key: trusted_public_key,
-        vbmeta_digest,
         capabilities,
         rollback_index,
         page_size,
