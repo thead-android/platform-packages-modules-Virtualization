2ed3b252d008f2e19b531cff902b69717043c29d
Frederick Mayle <fmayle@google.com>
2025-08-27 16:41:51 -0700

    Revert "Try to avoid the invalid vm config file"
    Revert submission 35193375
    
    Reason for revert: http://b/441586847
    
    Reverted changes: /q/submissionid:35193375
    
    Change-Id: I8042dff44ee2d4c2bfeb258c7231a640862b8244

--- libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
+++ libs/framework-virtualization/src/android/system/virtualmachine/VirtualMachine.java
@@ -113,7 +113,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -571,6 +570,7 @@ public class VirtualMachine implements AutoCloseable {
         try {
             VirtualizationService virtualizationService = VirtualizationService.getInstance();
             VirtualMachine vm = new VirtualMachine(context, name, config, virtualizationService);
+            config.serialize(vm.mConfigFilePath);
             try {
                 vm.mInstanceFilePath.createNewFile();
             } catch (IOException e) {
@@ -633,8 +633,6 @@ public class VirtualMachine implements AutoCloseable {
                             "failed to create encrypted storage partition", e);
                 }
             }
-            // persist the config only after we pass all the checks above.
-            config.serialize(vm.mConfigFilePath);
             return vm;
         } catch (VirtualMachineException | RuntimeException e) {
             // If anything goes wrong, delete any files created so far and the VM's directory
@@ -658,19 +656,7 @@ public class VirtualMachine implements AutoCloseable {
             return null;
         }
         File configFilePath = new File(thisVmDir, CONFIG_FILE);
-        VirtualMachineConfig config = null;
-        try {
-            config = VirtualMachineConfig.from(configFilePath);
-            Objects.requireNonNull(config);
-        } catch (VirtualMachineException e) {
-            Log.e(
-                    TAG,
-                    "Failed to load virtual machine config from "
-                            + configFilePath
-                            + ". It may be corrupted: "
-                            + e);
-            return null;
-        }
+        VirtualMachineConfig config = VirtualMachineConfig.from(configFilePath);
         VirtualMachine vm =
                 new VirtualMachine(context, name, config, VirtualizationService.getInstance());
 
@@ -2051,7 +2037,6 @@ public class VirtualMachine implements AutoCloseable {
      *
      * <p>NOTE: Modification of the encrypted storage size is restricted to expansion only and is an
      * irreversible operation.
-     *
      * <p>NOTE: This method may block and should not be called on the main thread.
      *
      * @return the old config
