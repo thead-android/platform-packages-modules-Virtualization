df80d928137a660b97fe23359e4c199791198ba8
Armelle Laine <armellel@google.com>
2025-08-29 09:00:17 +0000

    guest: trusty: test_vm: fix setup scripts
    - use killall for a more robust cleanup
    - use port 0 on the VMs for TipcPort
    
    Bug: 367423387
    Flag: EXEMPT bug fix
    Test: build and run  qemu_trusty_arm64-trunk_staging-userdebug target
    Change-Id: Iaeb07cb256d4c8e48656a2d6157b3f257dff109a
    Signed-off-by: Armelle Laine <armellel@google.com>

--- guest/trusty/test_vm/trusty-ut-ctrl.sh
+++ guest/trusty/test_vm/trusty-ut-ctrl.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-/data/local/tmp/trusty_test_vm/trusty-ut-ctrl -D VSOCK:${2:-$(getprop trusty.test_vm.vm_cid)}:1 $1
+/data/local/tmp/trusty_test_vm/trusty-ut-ctrl -D VSOCK:${2:-$(getprop trusty.test_vm.vm_cid)}:0 $1
