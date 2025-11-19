d50bcbaf5e64e7a2e12dbc1fd9a3468da36d19d7
Jaewan Kim <jaewan@google.com>
2025-11-19 18:08:43 +0900

    debian/guest_agents: Split features and gRPC logic, and add Android.bp
    This refactors shutdown_runner, storage_balloon_agent, and
    forwarder_guest_launcher to add Android.bp for their functionality
    without gRPC logic. Android.bp will be used to build the AIDL based
    guest agent.
    
    This is the first step toward soong generated guest agent.
    
    High level overview.
    --> Step 1) Split features from gRPC logic to be reused with AIDL
      - Step 2) Add soong build rule for gRPC agnostic guest agent.
                (i.e. forwarder_guest)
      - Step 3) Soong generates CIDATA.iso, and bundle it with virt apex.
      - Step 4) Create a new AIDL based guest agent and connect it with
                TerminalApp.
    
    Bug: 433849680
    Bug: 433848730
    Flag: EXEMPT PURE_REFACTOR
    Test: Build modules with both soong and cargo
    Change-Id: Id7f333a03355a9d2938b9f8335b6a00d7a20b876

--- guest/forwarder_guest_launcher/src/main.rs
+++ guest/forwarder_guest_launcher/src/main.rs
@@ -14,34 +14,18 @@
 
 //! Launcher of forwarder_guest
 
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use debian_service::debian_service_client::DebianServiceClient;
 use debian_service::{ActivePort, QueueOpeningRequest, ReportVmActivePortsRequest};
 use log::{debug, error};
 use std::collections::HashMap;
-use std::process::Stdio;
-use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::process::Command;
 use tokio::try_join;
 use tonic::transport::{Channel, Endpoint};
 use tonic::Request;
 
 mod debian_service {
     tonic::include_proto!("com.android.virtualization.terminal.proto");
-}
-
-const NON_PREVILEGED_PORT_RANGE_START: i32 = 1024;
-const TTYD_PORT: i32 = 7681;
-const TCPSTATES_STATE_CLOSE: &str = "CLOSE";
-const TCPSTATES_STATE_LISTEN: &str = "LISTEN";
-
-#[derive(Debug)]
-struct TcpState {
-    lport: i32,
-    rport: i32,
-    comm: String,
-    newstate: String,
 }
 
 #[derive(Parser)]
@@ -52,9 +36,7 @@ pub struct Args {
     grpc_port_file: String,
 }
 
-async fn process_forwarding_request_queue(
-    mut client: DebianServiceClient<Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn process_forwarding_request_queue(mut client: DebianServiceClient<Channel>) -> Result<()> {
     let cid = vsock::get_local_cid().context("Failed to get CID of VM")?;
     let mut res_stream = client
         .open_forwarding_request_queue(Request::new(QueueOpeningRequest { cid: cid as i32 }))
@@ -71,24 +53,22 @@ async fn process_forwarding_request_queue(
             &tcp_port, &vsock_port
         );
 
-        let _ = Command::new("forwarder_guest")
-            .arg("--local")
-            .arg(format!("127.0.0.1:{}", tcp_port))
-            .arg("--remote")
-            .arg(format!("vsock:2:{}", vsock_port))
-            .spawn();
+        forwarder_guest_launcher::forward_port(tcp_port.try_into().unwrap(), vsock_port);
     }
-    Err(anyhow!("process_forwarding_request_queue is terminated").into())
+    Err(anyhow!("process_forwarding_request_queue is terminated"))
 }
 
 async fn send_active_ports_report(
-    listening_ports: HashMap<i32, ActivePort>,
+    listening_ports: &HashMap<u16, String>,
     client: &mut DebianServiceClient<Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<()> {
+    let ports = listening_ports
+        .iter()
+        .map(|(port, comm)| ActivePort { port: *port as i32, comm: comm.clone() })
+        .collect();
+
     let res = client
-        .report_vm_active_ports(Request::new(ReportVmActivePortsRequest {
-            ports: listening_ports.values().cloned().collect(),
-        }))
+        .report_vm_active_ports(Request::new(ReportVmActivePortsRequest { ports }))
         .await?
         .into_inner();
     if res.success {
@@ -99,93 +79,16 @@ async fn send_active_ports_report(
     Ok(())
 }
 
-fn is_forwardable_port(port: i32) -> bool {
-    port >= NON_PREVILEGED_PORT_RANGE_START && port != TTYD_PORT
-}
-
-async fn report_active_ports(
-    mut client: DebianServiceClient<Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new("stdbuf")
-        .arg("-oL")
-        .arg("/usr/sbin/tcpstates-libbpf")
-        .stdout(Stdio::piped())
-        .spawn()?;
-    let stdout = cmd.stdout.take().context("Failed to get stdout of tcpstates")?;
-    let mut lines = BufReader::new(stdout).lines();
-    let header_line = lines.next_line().await?.ok_or(anyhow!("Failed to get header line"))?;
-    let header: Vec<_> = header_line.split_whitespace().collect();
-    let lport = header
-        .iter()
-        .position(|col| *col == "LPORT")
-        .ok_or(anyhow!("Failed to find LPORT from header"))?;
-    let rport = header
-        .iter()
-        .position(|col| *col == "RPORT")
-        .ok_or(anyhow!("Failed to find RPORT from header"))?;
-    let comm = header
-        .iter()
-        .position(|col| *col == "COMM")
-        .ok_or(anyhow!("Failed to find COMM from header"))?;
-    let newstate = header
-        .iter()
-        .position(|col| *col == "NEWSTATE")
-        .ok_or(anyhow!("Failed to find NEWSTATE from header"))?;
-
-    debug!("Collecting already opened ports");
-    let mut listening_ports: HashMap<_, _> = linux::net::get_listening_tcp4_ports_from_localhost()?
-        .into_iter()
-        .filter(|(x, _)| is_forwardable_port((*x).into()))
-        .map(|(port, comm)| {
-            debug!("Port {port} is already opened by {comm:?}");
-            (port.into(), ActivePort { port: port.into(), comm })
-        })
-        .collect();
-    send_active_ports_report(listening_ports.clone(), &mut client).await?;
-
-    debug!("Starting monitoring ports");
-    while let Some(line) = lines.next_line().await? {
-        let items: Vec<_> = line.split_whitespace().collect();
-        let state = TcpState {
-            lport: items
-                .get(lport)
-                .ok_or(anyhow!("Failed to find LPORT"))?
-                .parse()
-                .context("Invalid LPORT format")?,
-            rport: items
-                .get(rport)
-                .ok_or(anyhow!("Failed to find RPORT"))?
-                .parse()
-                .context("Invalid RPORT format")?,
-            comm: items.get(comm).ok_or(anyhow!("Failed to find COMM"))?.to_string(),
-            newstate: items.get(newstate).ok_or(anyhow!("Failed to find NEWSTATE"))?.to_string(),
-        };
-        if !is_forwardable_port(state.lport) {
-            continue;
-        }
-        if state.rport > 0 {
-            continue;
-        }
-        match state.newstate.as_str() {
-            TCPSTATES_STATE_LISTEN => {
-                debug!("New listening port {} by {}", state.lport, state.comm);
-                listening_ports
-                    .insert(state.lport, ActivePort { port: state.lport, comm: state.comm });
-            }
-            TCPSTATES_STATE_CLOSE => {
-                debug!("Listening port {} by {} is now closed", state.lport, state.comm);
-                listening_ports.remove(&state.lport);
-            }
-            _ => continue,
-        }
-        send_active_ports_report(listening_ports.clone(), &mut client).await?;
-    }
-
-    Err(anyhow!("report_active_ports is terminated").into())
+async fn report_active_ports(mut client: DebianServiceClient<Channel>) -> Result<()> {
+    forwarder_guest_launcher::monitor_active_ports(async |ports| {
+        send_active_ports_report(ports, &mut client).await
+    })
+    .await?;
+    Err(anyhow!("report_active_ports is terminated"))
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     env_logger::builder().filter_level(log::LevelFilter::Debug).init();
     debug!("Starting forwarder_guest_launcher");
     let args = Args::parse();
--- guest/storage_balloon_agent/src/main.rs
+++ guest/storage_balloon_agent/src/main.rs
@@ -15,7 +15,6 @@
 //! gRPC daemon for the storage ballooning feature.
 
 use anyhow::anyhow;
-use anyhow::Context;
 use anyhow::Result;
 use api::debian_service_client::DebianServiceClient;
 use api::StorageBalloonQueueOpeningRequest;
@@ -24,7 +23,7 @@ use clap::Parser;
 use log::debug;
 use log::error;
 use log::info;
-use nix::sys::statvfs::statvfs;
+use storage_balloon_agent::do_storage_ballooning;
 pub mod api {
     tonic::include_proto!("com.android.virtualization.terminal.proto");
 }
@@ -40,75 +39,6 @@ pub struct Args {
     #[arg(long)]
     #[arg(alias = "grpc_port_file")]
     grpc_port_file: String,
-}
-
-// Calculates how many blocks to be reserved.
-fn calculate_clusters_count(guest_available_bytes: u64) -> Result<u64> {
-    let stat = statvfs("/").context("failed to get statvfs")?;
-    let fr_size = stat.fragment_size() as u64;
-
-    if fr_size == 0 {
-        return Err(anyhow::anyhow!("fragment size is zero, fr_size: {}", fr_size));
-    }
-
-    let total = fr_size.checked_mul(stat.blocks() as u64).context(format!(
-        "overflow in total size calculation, fr_size: {}, blocks: {}",
-        fr_size,
-        stat.blocks()
-    ))?;
-
-    let free = fr_size.checked_mul(stat.blocks_available() as u64).context(format!(
-        "overflow in free size calculation, fr_size: {}, blocks_available: {}",
-        fr_size,
-        stat.blocks_available()
-    ))?;
-
-    let current_reserved_clusters_count = get_reserved_clusters()?;
-    let current_reserved_clusters_size =
-        current_reserved_clusters_count.checked_mul(fr_size).context(format!(
-            "overflow in calculate_reserved_clusters_size calculation,
-            current_reserved_clusters_count: {}, fr_size: {}",
-            current_reserved_clusters_count, fr_size
-        ))?;
-
-    let used = total.checked_sub(free + current_reserved_clusters_size).context(format!(
-        "underflow in used size calculation (free + current_reserved_clusters_size > total), which
-        should not happen, total: {}, free: {}, current_reserved_clusters_size: {}",
-        total, free, current_reserved_clusters_size
-    ))?;
-
-    let mut balloon_size_bytes = 0;
-    if total > guest_available_bytes + used {
-        balloon_size_bytes = total - guest_available_bytes - used;
-    }
-
-    let reserved_clusters_count = balloon_size_bytes.div_ceil(fr_size);
-
-    debug!("total: {total}, free: {free}, used: {used}, guest_avail: {guest_available_bytes}, balloon: {balloon_size_bytes}, clusters_count: {reserved_clusters_count}");
-
-    Ok(reserved_clusters_count)
-}
-
-fn set_reserved_clusters(clusters_count: u64) -> anyhow::Result<()> {
-    const ROOTFS_DEVICE_NAME: &str = "vda1";
-    std::fs::write(
-        format!("/sys/fs/ext4/{ROOTFS_DEVICE_NAME}/reserved_clusters"),
-        clusters_count.to_string(),
-    )
-    .context("failed to write reserved_clusters")?;
-    Ok(())
-}
-
-fn get_reserved_clusters() -> anyhow::Result<u64> {
-    const ROOTFS_DEVICE_NAME: &str = "vda1";
-    let path = format!("/sys/fs/ext4/{ROOTFS_DEVICE_NAME}/reserved_clusters");
-    let content =
-        std::fs::read_to_string(&path).with_context(|| format!("failed to read from {path}"))?;
-    let clusters_count = content
-        .trim()
-        .parse::<u64>()
-        .with_context(|| format!("failed to parse content of {path}: '{content}'"))?;
-    Ok(clusters_count)
 }
 
 #[tokio::main]
@@ -148,16 +78,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     while let Some(StorageBalloonRequestItem { available_bytes }) =
         res_stream.message().await.map_err(|e| anyhow!("failed to receive message: {:#}", e))?
     {
-        let clusters_count = match calculate_clusters_count(available_bytes) {
-            Ok(c) => c,
-            Err(e) => {
-                error!("failed to calculate cluster size to be reserved: {:#}", e);
-                continue;
-            }
-        };
-
-        if let Err(e) = set_reserved_clusters(clusters_count) {
-            error!("failed to set storage balloon size: {}", e);
+        if let Err(e) = do_storage_ballooning(available_bytes) {
+            error!("Failed to do storage ballooning. {e:?}");
         }
     }
 
