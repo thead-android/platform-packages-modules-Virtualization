ae625511a983c145db773190bf5d5b294dc21888
Jiyong Park <jiyong@google.com>
2025-11-28 13:16:50 +0900

    Follow-up on ag/37188987
    Use the extended property isImeVisible instead.
    
    Copy the original locking mechanism from the old activity.
    
    Bug: 448356313
    Flag: com.android.system.virtualmachine.flags.terminal_newui_jetpack
    Test: N/A
    Change-Id: Ia0045b595b429709d2b8f3f99c3ae9b9cb073f2b

--- android/TerminalApp/java/com/android/virtualization/terminal/new2/ui/MainActivity.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/new2/ui/MainActivity.kt
@@ -15,21 +15,39 @@
  */
 package com.android.virtualization.terminal.new2.ui
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.virtualization.terminal.R
 import com.android.virtualization.terminal.new2.ui.main.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lockOrientationIfNecessary()
         setContent {
             MaterialTheme {
                 val viewModel: MainViewModel = viewModel()
                 MainScreen(viewModel)
             }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        lockOrientationIfNecessary()
+    }
+
+    private fun lockOrientationIfNecessary() {
+        val hasHwQwertyKeyboard = resources.configuration.keyboard == Configuration.KEYBOARD_QWERTY
+        if (hasHwQwertyKeyboard) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        } else if (resources.getBoolean(R.bool.terminal_portrait_only)) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         }
     }
 }
