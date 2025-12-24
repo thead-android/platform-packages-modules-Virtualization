0d5f41358d6a4e1e8dc4a2d9bd74084188362f58
Mothukuri Likhitha <molikhitha@google.com>
2025-12-24 09:30:02 +0000

    libbssl_avf_nostd: Add Trusty build rules
    This change introduces the necessary `rules.mk` file to enable
    building the `libbssl_avf_nostd` library and its dependencies
    with the Trusty build system. This library is required to build
    the portable RKP TA.
    
    Bug: 467874147
    Test: build
    Flag: NONE changes applicable to Trusty builds only
    Change-Id: Ia0e8e3406991c5d2eecb3ad9c7b552395cf06d45

---
+++ libs/bssl/rules.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2026 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file is only for the Trusty build system
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MODULE_SRCS := $(LOCAL_DIR)/src/lib.rs
+
+MODULE_CRATE_NAME := bssl_avf
+
+MODULE_LIBRARY_DEPS += \
+	$(call FIND_CRATE,ciborium) \
+	$(call FIND_CRATE,coset) \
+	$(call FIND_CRATE,log) \
+	$(call FIND_CRATE,zeroize) \
+	packages/modules/Virtualization/libs/cborutil \
+	packages/modules/Virtualization/libs/bssl/error \
+	trusty/user/base/lib/bssl-sys-rust \
+
+include make/library.mk
