e3dc63eec34dba107f166f4bdfa98cfe4ca4662f
Jiyong Park <jiyong@google.com>
2026-01-31 09:03:34 +0000

    Terminal: bump the version from 4 to 5.
    The new terminal app no longer shares its app directory (/mnt/internal).
    
    Bug: 380997811
    Test: run ferrochrome
    Flag: EXEMPT BUGFIX
    Change-Id: I71c9386f34aef9f523145a053a0eac56413f5443

--- android/TerminalApp/java/com/android/virtualization/terminal/ImageArchive.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/ImageArchive.kt
@@ -231,7 +231,7 @@ internal class ImageArchive {
         private const val ARCHIVE_NAME = "images.tar.gz"
         private const val CIDATA_NAME = "cidata.iso"
         // TODO(b/403131508): externalize the version number
-        private const val VERSION_INT = 4_000_000 // 4.0.0
+        private const val VERSION_INT = 5_000_000 // 5.0.0
         private val BUILD_TAG =
             (VERSION_INT / 1_000_000 * 1_000_000).toString() // Use only the major version
         private val HOST_URL = "https://dl.google.com/android/ferrochrome/$BUILD_TAG"
