4e7600e3e8a30555ea1d1292135f4d33aef50800
Jiyong Park <jiyong@google.com>
2026-02-10 16:19:56 +0900

    Terminal: use imagevector for settings items
    Bug: 481701819
    Test: N/A
    Flag: EXEMPT BUGFIX
    Change-Id: Ia69fa8649122992c8a4134888d4346f758335f52

--- android/TerminalApp/java/com/android/virtualization/terminal/new2/ui/SettingsScreen.kt
+++ android/TerminalApp/java/com/android/virtualization/terminal/new2/ui/SettingsScreen.kt
@@ -32,6 +32,9 @@ import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Restore
+import androidx.compose.material.icons.filled.Security
+import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -64,6 +67,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -79,10 +83,10 @@ import com.android.virtualization.terminal.new2.core.VmController
 import com.android.virtualization.terminal.new2.ui.main.MainViewModel
 import kotlinx.coroutines.launch
 
-enum class SettingsDestination(val title: Int, val icon: Int) {
-    PortControl(R.string.settings_port_title, R.drawable.baseline_call_missed_outgoing_24),
-    Advanced(R.string.settings_advanced_title, R.drawable.ic_display),
-    Recovery(R.string.settings_recovery_title, R.drawable.baseline_settings_backup_restore_24),
+enum class SettingsDestination(val title: Int, val icon: ImageVector) {
+    PortControl(R.string.settings_port_title, Icons.Default.Security),
+    Advanced(R.string.settings_advanced_title, Icons.Default.Tune),
+    Recovery(R.string.settings_recovery_title, Icons.Default.Restore),
 }
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
@@ -184,7 +188,7 @@ fun SettingsListPane(
                 ListItem(
                     headlineContent = { Text(stringResource(item.title)) },
                     leadingContent = {
-                        Icon(painter = painterResource(item.icon), contentDescription = null)
+                        Icon(imageVector = item.icon, contentDescription = null)
                     },
                     modifier = Modifier.clickable { onItemClick(item) },
                     colors =
