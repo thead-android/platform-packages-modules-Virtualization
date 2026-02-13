b5837b1d97ce9182645087aef41ab2fc91c0bf72
Nikita Ioffe <ioffe@google.com>
2026-02-13 01:57:48 +0000

    Remove cfg!(llvpvm_change) check
    The flag was has been released 2 releases ago.
    
    Bug: 340506965
    Test: builds
    Test: presubmit
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: Ifb6863725f9515b93f8b1c01594375d6e4cd70e3

--- android/virtmgr/src/virtualmachine.rs
+++ android/virtmgr/src/virtualmachine.rs
@@ -960,20 +960,18 @@ fn maybe_create_reference_dt_overlay(
 
     let key_material;
     let mut untrusted_props = Vec::with_capacity(2);
-    if cfg!(llpvm_changes) {
-        untrusted_props.push((c"instance-id", &instance_id[..]));
-        let want_updatable = extract_want_updatable(config);
-        if want_updatable && secretkeeper::is_supported() {
-            // Let guest know that it can defer rollback protection to Secretkeeper by setting
-            // an empty property in untrusted node in DT. This enables Updatable VMs.
-            untrusted_props.push((c"defer-rollback-protection", &[]));
-            let sk: Strong<dyn aidl::ISecretkeeper> =
-                binder::wait_for_interface(SECRETKEEPER_IDENTIFIER)?;
-            if sk.getInterfaceVersion()? >= 2 {
-                let aidl::PublicKey { keyMaterial } = sk.getSecretkeeperIdentity()?;
-                key_material = keyMaterial;
-                trusted_props.push((c"secretkeeper_public_key", key_material.as_slice()));
-            }
+    untrusted_props.push((c"instance-id", &instance_id[..]));
+    let want_updatable = extract_want_updatable(config);
+    if want_updatable && secretkeeper::is_supported() {
+        // Let guest know that it can defer rollback protection to Secretkeeper by setting
+        // an empty property in untrusted node in DT. This enables Updatable VMs.
+        untrusted_props.push((c"defer-rollback-protection", &[]));
+        let sk: Strong<dyn aidl::ISecretkeeper> =
+            binder::wait_for_interface(SECRETKEEPER_IDENTIFIER)?;
+        if sk.getInterfaceVersion()? >= 2 {
+            let aidl::PublicKey { keyMaterial } = sk.getSecretkeeperIdentity()?;
+            key_material = keyMaterial;
+            trusted_props.push((c"secretkeeper_public_key", key_material.as_slice()));
         }
     }
 
