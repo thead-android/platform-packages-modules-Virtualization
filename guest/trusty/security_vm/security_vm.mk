72def7c687078431459aec01de99b8f4884a8a68
Alice Wang <aliceywang@google.com>
2026-02-25 10:22:26 +0000

    trusty-build: Remove redundant TRUSTY_SYSTEM_VM check in security_vm.mk
    The ifeq check for TRUSTY_SYSTEM_VM is redundant because this
    makefile is already conditionally included at a higher level. Removing
    this logic simplifies the file and ensures that when the makefile is
    present, all relevant artifacts are included as intended.
    
    Test: m
    Bug: 484874293
    Flag: build.AVF_TRUSTY_ENABLE_SYSTEM_VM_WITH_PLACEHOLDER_THAL
    Change-Id: Ia70eb39701573ba400b992dbdf39a275f67a9f3e

--- guest/trusty/security_vm/security_vm.mk
+++ guest/trusty/security_vm/security_vm.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-ifeq ($(findstring enabled, $(TRUSTY_SYSTEM_VM)),enabled)
-
 # This is the default set of packages to load the trusty system vm.
 # It can be overridden by device-specific configuration.
 TRUSTY_SYSTEM_VM_PRODUCT_PACKAGES ?= trusty_security_vm.elf \
@@ -27,5 +24,3 @@ TRUSTY_SYSTEM_VM_PRODUCT_PACKAGES ?= trusty_security_vm.elf \
 PRODUCT_PACKAGES += \
 	keymint_provisioning_tool \
 	$(TRUSTY_SYSTEM_VM_PRODUCT_PACKAGES) \
-
-endif
